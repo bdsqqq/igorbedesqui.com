eb6c335bb2b41e806883b0765b03179e533db9cb
bdsqqq <igorbedesqui@gmail.com>
2021-01-05 20:25:09 -0300

    feat: implemented typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
-import Nav from '../components/nav'
-
-export default function IndexPage() {
+export default function Home() {
   return (
     <div>
-      <Nav />
       <div className="py-20">
         <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
           Next.js + Tailwind CSS 2.0
         </h1>
       </div>
     </div>
-  )
+  );
 }
