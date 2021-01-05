b31c8728f6a098f622ffa0830cb36a1f53332aac
bdsqqq <igorbedesqui@gmail.com>
2021-01-05 20:26:24 -0300

    style: set darkmode to class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // 'media' or 'class'
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        'accent-1': '#333',
+        "accent-1": "#333",
       },
     },
   },
@@ -12,4 +12,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
