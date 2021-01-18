65294349cd35d6e0bdb50a86b17f1a76046b0d20
bdsqqq <igorbedesqui@gmail.com>
2021-01-18 19:11:09 -0300

    feat: updated i18n implementation and page styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,29 @@
-import useTranslation from "next-translate/useTranslation";
 import Trans from "next-translate/Trans";
 import Container from "../components/Container";
 
 export default function Home() {
-  const { t, lang } = useTranslation("common");
-  const title = t("title");
   return (
     <Container
       heading={
         <Trans
-          i18nKey="common:title"
-          components={[<span className=" font-bold" />]}
+          i18nKey="home:hero"
+          components={[
+            <span className="font-light text-sm md:text-2xl" />,
+            <br />,
+            <span className="font-bold" />,
+          ]}
         />
       }
       heroSrc="/images/giphy.gif"
     >
       <section className="max-w-5xl mx-auto  px-8 ">
         <div className="flex flex-col justify-center items-start max-w-4xl mx-auto mb-16 py-8">
-          <h1 className="font-bold text-3xl md:text-6xl tracking-tight mb-4 text-gray-700">
-            Lorem ipsum dolor sit. {lang}
+          <h1 className="text-2xl md:text-5xl tracking-tight mb-4 text-gray-700">
+            <Trans
+              i18nKey="home:about"
+              components={[<span className="font-bold" />]}
+            />
           </h1>
-          <h2 className="text-gray-600 mb-16">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Repudiandae suscipit quisquam atque quidem aut, dignissimos ut eaque
-            eos provident, tempora vitae sed optio at et aperiam a amet,
-            nesciunt cupiditate!
-          </h2>
         </div>
       </section>
       <div className="h-48 bg-oliver-500"></div>
