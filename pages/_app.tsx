939bc8d62b30efd4041cd7b521e39e30010e2f83
Igor Bedesqui <igorbedesqui@gmail.com>
2021-02-01 18:57:07 -0300

    build: removed next-themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
 import "../styles/index.css";
 import type { AppProps } from "next/app";
 
-import { ThemeProvider } from "next-themes";
-
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
