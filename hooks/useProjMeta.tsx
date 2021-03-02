cd514985f3160bb10ddd62cda21f7e82b9f8ebba
Igor Bedesqui <igorbedesqui@gmail.com>
2021-03-02 20:32:31 -0300

    feat: implemented arrays for roles & tools in meta

--- hooks/useProjMeta.tsx
+++ hooks/useProjMeta.tsx
@@ -2,9 +2,9 @@ function useProjMeta(projName: string) {
   const { t } = useTranslation(`projs/meta`);
   return {
     name: t(`${projName}.name`),
-    role: t(`${projName}.role`),
+    roles: t(`${projName}.roles`).split(","),
     type: t(`${projName}.type`),
-    tools: t(`${projName}.tools`),
+    tools: t(`${projName}.tools`).split(","),
     date: t(`${projName}.date`),
   };
 }
