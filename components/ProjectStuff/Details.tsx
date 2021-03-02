e2bf02107bc3f52f7e181fad71305e63e40ab314
Igor Bedesqui <igorbedesqui@gmail.com>
2021-03-02 20:42:19 -0300

    fix: fixed children usage

--- components/ProjectStuff/Details.tsx
+++ components/ProjectStuff/Details.tsx
@@ -2,12 +2,12 @@ interface DetailsBandProps {
   projName: string;
 }
 
-const DetailsBand: React.FC<DetailsBandProps> = ({ projName }) => {
+const DetailsBand: React.FC<DetailsBandProps> = ({ projName, children }) => {
   const { t } = useTranslation();
 
   return (
     <Band headline={{ bold: projName, thin: t("projs/detail:details") }}>
-      <div className="grid grid-cols-1fr12rem gap-4 pb-8">{Children}</div>
+      <div className="grid grid-cols-1fr12rem gap-4 pb-8">{children}</div>
     </Band>
   );
 };
@@ -15,6 +15,5 @@ const DetailsBand: React.FC<DetailsBandProps> = ({ projName }) => {
 export default DetailsBand;
 
 import useTranslation from "next-translate/useTranslation";
-import { Children } from "react";
 
 import Band from "../Band";
