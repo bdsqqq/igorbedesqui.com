f5c74d5887f1cb7ca70f5728e62411659a15dcca
Igor Bedesqui <igorbedesqui@gmail.com>
2021-03-10 21:29:26 -0300

    feat: updated DetailsBand to use the new Band headline styles properly

--- components/ProjectStuff/DetailsBand.tsx
+++ components/ProjectStuff/DetailsBand.tsx
@@ -3,17 +3,13 @@ interface DetailsBandProps {
 }
 
 const DetailsBand: React.FC<DetailsBandProps> = ({ projName, children }) => {
-  const { t } = useTranslation();
-
   return (
-    <Band headline={{ bold: projName, thin: t("projs/detail:details") }}>
+    <Band headline={{ bold: "-", thin: projName }}>
       <div className="grid grid-cols-1fr12rem gap-4 pb-8">{children}</div>
     </Band>
   );
 };
 
 export default DetailsBand;
-
-import useTranslation from "next-translate/useTranslation";
 
 import Band from "../Band";
