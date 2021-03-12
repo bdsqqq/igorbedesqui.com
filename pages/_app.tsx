c1cf0101e40b410a35e60e1acfac5281e553b5d8
Igor Bedesqui <igorbedesqui@gmail.com>
2021-03-12 18:31:10 -0300

    feat: implemented history context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import "../styles/index.css";
 import type { AppProps } from "next/app";
+import { HistoryProvider } from "../contexts/History";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <HistoryProvider>
+      <Component {...pageProps} />
+    </HistoryProvider>
+  );
 }
 
 export default MyApp;
