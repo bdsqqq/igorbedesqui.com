9b6432f0b6ae28ca6f0e203453f19577fa61a258
Igor Bedesqui <igorbedesqui@gmail.com>
2021-03-22 21:47:05 -0300

    feat: applied i18n to hey band, removed placeholder bands

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 export default function Home() {
   const { t } = useTranslation("home");
-  const issMeta = useProjMeta("iss");
-  const projsMeta = [issMeta, issMeta, issMeta];
 
   return (
     <>
@@ -21,74 +19,34 @@ export default function Home() {
         </HeroBand>
         <Band headline={{ bold: "01", thin: t("01Title") }}>
           <p className="text-xl md:text-3xl tracking-tight ">
-            <TransWithComps i18nKey="home:inAFewWords" />
+            <TransWithComps i18nKey="home:01Copy" />
           </p>
         </Band>
-        <Band
-          dark
-          headline={{ bold: "02", thin: t("02Title") }}
-          cta={{ target: "/p", text: t("02Cta") as string }}
-        >
-          <Projects projectsMeta={projsMeta} />
-        </Band>
-        <Band
-          gridless
-          id="something"
-          cta={{
-            target: "/example",
-            text: "CTA TEXT",
-          }}
-        >
-          <p className="text-4xl text-center italic font-light">
-            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod qui
-            facere voluptate! "
-          </p>
-        </Band>
-        <Band
-          headline={{ bold: "03", thin: "Gallery" }}
-          cta={{
-            target: "/example",
-            text: "CTA TEXT",
-          }}
-        >
-          <div className="grid grid-cols-1fr12rem gap-4 pb-8">
-            <Image
-              alt="sunset at montmajour"
-              src="/images/background.jpg"
-              width={700}
-              height={500}
-            />
-            <Image
-              alt="sunset at montmajour"
-              src="/images/background.jpg"
-              width={700}
-              height={500}
-            />
-            <Image
-              alt="sunset at montmajour"
-              src="/images/background.jpg"
-              width={700}
-              height={500}
-            />
-            <Image
-              alt="sunset at montmajour"
-              src="/images/background.jpg"
-              width={700}
-              height={500}
-            />
-          </div>
-        </Band>
-        <Band
-          headline={{ bold: "Hey", thin: `let's connect!` }}
-          cta={{
-            target: "#",
-            text: "back to the top",
-          }}
-        >
+        <Band headline={{ bold: t("hey"), thin: t("heyTitle") }}>
           <p className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-            laborum porro distinctio obcaecati sequi dolorum aperiam ipsam
-            molestiae est beatae!
+            <TransWithComps
+              i18nKey="home:heyCopy"
+              extraComponents={{
+                twitter: (
+                  <ExternalLink
+                    aClassList="font-bold opacity-90 hover:opacity-100 focus:opacity-100 transition transition-all"
+                    href="https://twitter.com/Igorbdsq"
+                  ></ExternalLink>
+                ),
+                github: (
+                  <ExternalLink
+                    aClassList="font-bold opacity-90 hover:opacity-100 focus:opacity-100 transition transition-all"
+                    href="https://github.com/bdsqqq"
+                  ></ExternalLink>
+                ),
+                email: (
+                  <ExternalLink
+                    aClassList="font-bold opacity-90 hover:opacity-100 focus:opacity-100 transition transition-all"
+                    href="mailto:igorbedesqui@gmail.com"
+                  ></ExternalLink>
+                ),
+              }}
+            />
           </p>
         </Band>
       </Container>
@@ -97,12 +55,10 @@ export default function Home() {
 }
 
 import Head from "next/head";
-import Image from "next/image";
 import useTranslation from "next-translate/useTranslation";
-import useProjMeta from "../hooks/useProjMeta";
 
 import Band from "../components/Band";
 import Container from "../components/Container";
 import TransWithComps from "../components/i18nStuff/TransWithComps";
-import Projects from "../components/ProjectStuff/Projects";
 import HeroBand from "../components/HeroBand";
+import ExternalLink from "../components/ExternalLink";
