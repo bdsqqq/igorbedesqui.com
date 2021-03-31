0619960fea636be547cd35361ebdcf6dc4a6eac4
Igor Bedesqui <igorbedesqui@gmail.com>
2021-03-31 20:38:13 -0300

    feat: added favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,32 @@ class MyDocument extends Document {
             type="font/woff2"
             crossOrigin="anonymous"
           />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="favicon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#fffcf5" />
+          <meta name="theme-color" content="#473539" />
         </Head>
         <body>
           <Main />
