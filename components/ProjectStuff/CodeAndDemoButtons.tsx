bf550ad98d442bce6b68b199a11b0acd4a98c778
Igor Bedesqui <igorbedesqui@gmail.com>
2021-07-19 21:52:06 -0300

    feat: updated ExternalLink to UnstyledLink

--- components/ProjectStuff/CodeAndDemoButtons.tsx
+++ components/ProjectStuff/CodeAndDemoButtons.tsx
@@ -13,7 +13,7 @@ const CodeAndDemoButtons: React.FC<CodeAndDemoButtonsProps> = ({
   return (
     <div className="flex">
       {codeUrl && (
-        <ExternalLink aClassList={buttonClasses} href={codeUrl}>
+        <UnstyledLink className={buttonClasses} href={codeUrl}>
           <span className="mr-2">{t("code")}</span>
           <span className="sr-only">Github</span>
           <svg className="h-5 w-5" viewBox="0 0 24 24">
@@ -27,10 +27,10 @@ const CodeAndDemoButtons: React.FC<CodeAndDemoButtonsProps> = ({
               <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
             </g>
           </svg>
-        </ExternalLink>
+        </UnstyledLink>
       )}
       {demoUrl && (
-        <ExternalLink aClassList={buttonClasses} href={demoUrl}>
+        <UnstyledLink className={buttonClasses} href={demoUrl}>
           <span className="mr-2">{t("demo")}</span>
           <svg className="h-5 w-5" viewBox="0 0 24 24">
             <g
@@ -43,7 +43,7 @@ const CodeAndDemoButtons: React.FC<CodeAndDemoButtonsProps> = ({
               <path d="M5 3l14 9-14 9V3z" />
             </g>
           </svg>
-        </ExternalLink>
+        </UnstyledLink>
       )}
     </div>
   );
@@ -51,5 +51,5 @@ const CodeAndDemoButtons: React.FC<CodeAndDemoButtonsProps> = ({
 
 export default CodeAndDemoButtons;
 
-import ExternalLink from "@/components/ExternalLink";
+import UnstyledLink from "@/components/UnstyledLink";
 import useTranslation from "next-translate/useTranslation";
