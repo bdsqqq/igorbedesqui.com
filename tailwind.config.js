7320af2d37f369676c198a28cd6c5d8b1b909de8
Igor Bedesqui <igorbedesqui@gmail.com>
2021-07-19 21:06:39 -0300

    refactor: removed unused styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const { spacing, fontFamily } = require("tailwindcss/defaultTheme");
+const { fontFamily } = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 module.exports = {
@@ -43,28 +43,7 @@ module.exports = {
           "100%": { opacity: 1 },
         },
       },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            color: theme("colors.gray.700"),
-            a: {
-              color: theme("colors.blue.500"),
-              "&:hover": {
-                color: theme("colors.blue.700"),
-              },
-              code: { color: theme("colors.blue.400") },
-            },
-            "h2,h3,h4": {
-              "scroll-margin-top": spacing[32],
-            },
-            code: { color: theme("colors.pink.500") },
-            "blockquote p:first-of-type::before": false,
-            "blockquote p:last-of-type::after": false,
-          },
-        },
-      }),
     },
   },
   variants: {},
-  plugins: [require("@tailwindcss/typography")],
 };
