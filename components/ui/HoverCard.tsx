fa64b0dee967afeb38249c178d86356a344e514b
Igor Bedesqui <igorbedesqui@gmail.com>
2021-10-18 19:11:47 -0300

    feat: updated delays and added animations

--- components/ui/HoverCard.tsx
+++ components/ui/HoverCard.tsx
@@ -5,9 +5,9 @@ interface HoverCard {
 }
 
 const HoverCard: React.FC<HoverCard> = ({ children, content, dark }) => (
-  <HoverCardPrimitive.Root>
+  <HoverCardPrimitive.Root openDelay={200} closeDelay={100}>
     <HoverCardPrimitive.Trigger>{children}</HoverCardPrimitive.Trigger>
-    <HoverCardPrimitive.Content
+    <HoverCardContent
       side="top"
       className={`relative p-4 w-64 rounded-sm shadow-md border ${
         !dark
@@ -23,8 +23,45 @@ const HoverCard: React.FC<HoverCard> = ({ children, content, dark }) => (
             : "text-sandDark-sand3 stroke-sandDark-sand7"
         }`}
       />
-    </HoverCardPrimitive.Content>
+    </HoverCardContent>
   </HoverCardPrimitive.Root>
 );
 
 export default HoverCard;
+
+const scaleIn = keyframes({
+  "0%": { opacity: 0, transform: "scale(0)" },
+  "100%": { opacity: 1, transform: "scale(1)" },
+});
+
+const scaleOut = keyframes({
+  "0%": { opacity: 1, transform: "scale(1)" },
+  "100%": { opacity: 0, transform: "scale(0)" },
+});
+
+const slideDown = keyframes({
+  "0%": { opacity: 0, transform: "translateY(-5px)" },
+  "100%": { opacity: 1, transform: "translateY(0)" },
+});
+
+const slideUp = keyframes({
+  "0%": { opacity: 0, transform: "translateY(5px)" },
+  "100%": { opacity: 1, transform: "translateY(0)" },
+});
+
+const HoverCardContent = styled(HoverCardPrimitive.Content, {
+  '&[data-side="top"]': { animationName: slideUp, scaleIn },
+  '&[data-side="bottom"]': { animationName: slideDown, scaleIn },
+
+  '&[data-state="closed"]': {
+    animationName: scaleOut,
+    animationTimingFunction: "cubic-bezier(0.2, 0, 1, 0.9)",
+  },
+
+  animationDuration: "0.15s",
+  animationTimingFunction: "cubic-bezier(0, 0, 0.38, 0.9)",
+
+  transformOrigin: "var(--radix-hover-card-content-transform-origin)",
+});
+
+import { styled, keyframes } from "@stitches/react";
