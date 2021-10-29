57b50dc0de9b4f5e1462c0bdb556bdf11c43f0fc
Igor Bedesqui <igorbedesqui@gmail.com>
2021-10-29 01:18:01 -0300

    feat: added BackToTop button

---
+++ components/ui/BackToTop.tsx
@@ -0,0 +1,91 @@
+const BackToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset >= 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <Button
+      visible={isVisible}
+      tabIndex={isVisible ? 0 : -1}
+      onClick={scrollToTop}
+    >
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 15 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M3.13523 8.84197C3.3241 9.04343 3.64052 9.05363 3.84197 8.86477L7.5 5.43536L11.158 8.86477C11.3595 9.05363 11.6759 9.04343 11.8648 8.84197C12.0536 8.64051 12.0434 8.32409 11.842 8.13523L7.84197 4.38523C7.64964 4.20492 7.35036 4.20492 7.15803 4.38523L3.15803 8.13523C2.95657 8.32409 2.94637 8.64051 3.13523 8.84197Z"
+          fill="currentColor"
+          fillRule="evenodd"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+    </Button>
+  );
+};
+
+const Button = styled("button", {
+  width: "3rem",
+  height: "3rem",
+  borderRadius: 9999,
+  padding: "0.75rem",
+
+  color: "$sandDark12",
+  backgroundColor: "$sandDark3",
+  borderStyle: "solid",
+  borderWidth: "1px",
+  borderColor: "$sandDark7",
+
+  display: "hidden",
+  opacity: "0",
+  transform: "scale(0) translate(0, 5rem)",
+
+  transitionDuration: "150ms",
+  transitionTimingFunction: "cubic-bezier(0.4, 0.14, 0.3, 1)",
+
+  variants: {
+    visible: {
+      true: {
+        display: "block",
+        opacity: "1",
+        transform: "scale(1) translate(0, 0)",
+        transitionTimingFunction: "cubic-bezier(0, 0, 0.3, 1)",
+      },
+    },
+  },
+
+  "&:hover": {
+    backgroundColor: "$sandDark4",
+    borderColor: "$sandDark8",
+  },
+
+  "&:active, &:focus-visible, &:focus": {
+    backgroundColor: "$sandDark5",
+    borderColor: "$sandDark8",
+  },
+});
+
+export default BackToTop;
+
+import { styled } from "stitches.config";
+import { useState, useEffect } from "react";
