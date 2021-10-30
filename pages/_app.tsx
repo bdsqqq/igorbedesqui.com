4f10d135ef5e01b9694f54d8a5d974cbae9df28b
Igor Bedesqui <igorbedesqui@gmail.com>
2021-10-29 22:39:39 -0300

    feat: updated global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,17 +9,12 @@ const globalStyles = globalCss({
       "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,U+FEFF, U+FFFD",
   },
 
-  "&::selection": {
-    backgroundColor: "mauveDark5",
-    color: "mauveDark12",
-  },
-
   "*": {
     padding: 0,
     margin: 0,
     boxSizing: "border-box",
 
-    outlineRing: "$mauveDark8",
+    outlineRing: "$mauve8",
   },
 
   "html, body": {
@@ -30,6 +25,11 @@ const globalStyles = globalCss({
     minWidth: "360px",
     scrollBehavior: "smooth",
     fontSmooth: "auto",
+  },
+
+  "::selection": {
+    backgroundColor: "$crimson9",
+    color: "$mauve1",
   },
 
   /* Remove Safari input shadow on mobile */
@@ -43,13 +43,11 @@ const globalStyles = globalCss({
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
   return (
-    <div className="bg-mauve-mauve1">
-      <HistoryProvider>
-        <div className="fadeIn">
-          <Component {...pageProps} />
-        </div>
-      </HistoryProvider>
-    </div>
+    <HistoryProvider>
+      <div className="fadeIn">
+        <Component {...pageProps} />
+      </div>
+    </HistoryProvider>
   );
 }
 
