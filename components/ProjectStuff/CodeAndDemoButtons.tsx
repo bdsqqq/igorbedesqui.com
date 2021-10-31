2d63a6f067d09ad1ad42f39e0cf65d4d27f19f73
Igor Bedesqui <igorbedesqui@gmail.com>
2021-10-31 00:02:43 -0300

    feat: stitched styles

--- components/ProjectStuff/CodeAndDemoButtons.tsx
+++ components/ProjectStuff/CodeAndDemoButtons.tsx
@@ -8,15 +8,14 @@ const CodeAndDemoButtons: React.FC<CodeAndDemoButtonsProps> = ({
   demoUrl,
 }) => {
   const { t } = useTranslation("common");
-  const buttonClasses =
-    "flex items-center text-lg py-1 px-2 align-middle bg-mauveDark-mauve3 text-mauveDark-mauve12 shadow-lg rounded mr-2 hover:bg-mauveDark-mauve4 hover:shadow-xl focus:bg-mauveDark-mauve5 active:bg-mauveDark-mauve5 focus:shadow-xl transition-all cursor-pointer";
+
   return (
-    <div className="flex">
+    <Box css={{ display: "flex", gap: "1rem" }} className={darkTheme}>
       {codeUrl && (
-        <UnstyledLink className={buttonClasses} href={codeUrl}>
-          <span className="mr-2">{t("code")}</span>
+        <LinkButton href={codeUrl}>
+          <span>{t("code")}</span>
           <span className="sr-only">Github</span>
-          <svg className="h-5 w-5" viewBox="0 0 24 24">
+          <Svg viewBox="0 0 24 24">
             <g
               fill="none"
               stroke="currentColor"
@@ -26,13 +25,13 @@ const CodeAndDemoButtons: React.FC<CodeAndDemoButtonsProps> = ({
             >
               <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
             </g>
-          </svg>
-        </UnstyledLink>
+          </Svg>
+        </LinkButton>
       )}
       {demoUrl && (
-        <UnstyledLink className={buttonClasses} href={demoUrl}>
-          <span className="mr-2">{t("demo")}</span>
-          <svg className="h-5 w-5" viewBox="0 0 24 24">
+        <LinkButton href={demoUrl}>
+          <span>{t("demo")}</span>
+          <Svg viewBox="0 0 24 24">
             <g
               fill="none"
               stroke="currentColor"
@@ -42,14 +41,21 @@ const CodeAndDemoButtons: React.FC<CodeAndDemoButtonsProps> = ({
             >
               <path d="M5 3l14 9-14 9V3z" />
             </g>
-          </svg>
-        </UnstyledLink>
+          </Svg>
+        </LinkButton>
       )}
-    </div>
+    </Box>
   );
 };
 
+const Svg = styled("svg", {
+  width: "1.25rem",
+  height: "1.25rem",
+});
+
 export default CodeAndDemoButtons;
 
-import UnstyledLink from "@/components/ui/primitives/UnstyledLink";
+import { darkTheme, styled } from "stitches.config";
+import { LinkButton } from "@/ui/Button";
+import { Box } from "@/ui/primitives";
 import useTranslation from "next-translate/useTranslation";
