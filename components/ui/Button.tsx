9c6c55192b56147583006c30a91fa6c223960a31
Igor Bedesqui <igorbedesqui@gmail.com>
2021-10-31 00:01:52 -0300

    feat: added Button components

---
+++ components/ui/Button.tsx
@@ -0,0 +1,71 @@
+const buttonCss: Stitches.CSS<typeof config> = {
+  // mini reset
+  cursor: "pointer",
+  userSelect: "none",
+  appearance: "none",
+
+  display: "inline-flex",
+  alignItems: "center",
+  gap: "0.5rem",
+
+  borderStyle: "solid",
+  borderWidth: "1px",
+  borderColor: "transparent",
+  borderRadius: "0.25rem",
+
+  fontSize: "1.125rem" /* 18px */,
+  lineHeight: "1.75rem" /* 28px */,
+
+  py: "0.25rem",
+  px: "0.5rem",
+  verticalAlign: "middle",
+
+  transitionDuration: "150ms",
+  transitionTimingFunction: "cubic-bezier(0.4, 0.14, 0.3, 1)",
+
+  variants: {
+    color: {
+      mauve: {
+        color: "$mauve12",
+        backgroundColor: "$mauve3",
+        borderColor: "$mauve7",
+
+        "&:hover, &:focus-within": {
+          backgroundColor: "$mauve5",
+          borderColor: "$mauve8",
+        },
+
+        "&:active": {
+          backgroundColor: "$mauve6",
+        },
+      },
+      crimson: {
+        color: "$crimson12",
+        backgroundColor: "$crimson3",
+        borderColor: "$crimson7",
+
+        "&:hover, &:focus-within": {
+          backgroundColor: "$crimson4",
+          borderColor: "$crimson8",
+        },
+        "&:active": {
+          backgroundColor: "$crimson5",
+        },
+      },
+    },
+  },
+
+  defaultVariants: {
+    color: "mauve",
+  },
+};
+
+const Button = styled("button", buttonCss);
+
+const LinkButton = styled(UnstyledLink, buttonCss);
+
+export { Button, LinkButton };
+
+import { styled, config } from "stitches.config";
+import { UnstyledLink } from "./primitives";
+import type * as Stitches from "@stitches/react";
