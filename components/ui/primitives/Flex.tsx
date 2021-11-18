5cce13813eea452278ccfe4e1e406987aa39e524
Igor Bedesqui <igorbedesqui@gmail.com>
2021-11-17 21:51:18 -0300

    feat: added Flex UI primitive

---
+++ components/ui/primitives/Flex.tsx
@@ -0,0 +1,7 @@
+const Flex = styled("div", {
+  display: "flex",
+});
+
+export default Flex;
+
+import { styled } from "stitches.config";
--- components/ui/primitives/index.tsx
+++ components/ui/primitives/index.tsx
@@ -1,7 +1,8 @@
 import { default as Box } from "./Box";
+import { default as Flex } from "./Flex";
 import { default as Span } from "./Span";
 import { default as FABContainer } from "./FABContainer";
 import { default as SrOnly } from "./SrOnly";
 import { default as UnstyledLink } from "./UnstyledLink";
 
-export { Box, Span, FABContainer, SrOnly, UnstyledLink };
+export { Box, Flex, Span, FABContainer, SrOnly, UnstyledLink };
