e41a78d594f8307bf9076cffdc15e32f009eae6b
Igor Bedesqui <igorbedesqui@gmail.com>
2021-11-24 11:56:54 -0300

    refactor: moved ChangeLang to i18nStuff

--- components/i18nStuff/ChangeLang.tsx
+++ components/i18nStuff/ChangeLang.tsx
@@ -100,7 +100,7 @@ import { Span } from "@/ui/primitives";
 
 import useTranslation from "next-translate/useTranslation";
 import setLanguage from "next-translate/setLanguage";
-import i18nConfig from "../i18n.json";
+import i18nConfig from "../../i18n.json";
 
 import en from "@/locales/en/common.json";
 import pt from "@/locales/pt/common.json";
