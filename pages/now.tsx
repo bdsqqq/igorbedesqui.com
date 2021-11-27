d709c21a1e359aa62119001e8564e6ecf578bf7f
Igor Bedesqui <igorbedesqui@gmail.com>
2021-11-27 17:06:45 -0300

    feat: updated now page

--- pages/now.tsx
+++ pages/now.tsx
@@ -15,20 +15,58 @@ export default function Home() {
       <Container backable key="index">
         <Band gridless id="main">
           <Box css={{ maxWidth: "60ch", marginX: "auto" }}>
-            <h1 className="text-4xl font-semibold">What I'm doing now</h1>
-            <Ul>
+            <Text as="h1" presetStyle="heading">
+              What I'm doing now
+            </Text>
+            <Flex
+              as="ul"
+              css={{
+                flexDirection: "column",
+                gap: "$spacing-03",
+
+                marginTop: "$spacing-06",
+                marginBottom: "$spacing-11",
+
+                listStyleType: "circle",
+                listStylePosition: "outside",
+              }}
+            >
               {thingsImDoing.map((thing) => {
-                return <li key={thing}>{thing}</li>;
+                return (
+                  <Text
+                    as="li"
+                    css={{
+                      fontSize: "$lg",
+                      lineHeight: "$lg",
+                    }}
+                    key={thing}
+                  >
+                    {thing}
+                  </Text>
+                );
               })}
-            </Ul>
+            </Flex>
 
-            <p>Lorem ipsum dolor sit amet consectetur.</p>
+            <Text
+              as="p"
+              css={{
+                marginBottom: "$spacing-07",
+              }}
+            >
+              <TransWithComps i18nKey="now:bottomText" />
+            </Text>
 
-            <hr />
+            <Hr
+              css={{
+                width: "24rem",
+                marginX: "auto",
+                marginBottom: "$spacing-07",
+              }}
+            />
 
-            <p>
+            <Text as="p">
               <TransWithComps i18nKey="now:lastUpdated" />
-            </p>
+            </Text>
           </Box>
         </Band>
       </Container>
@@ -36,10 +74,7 @@ export default function Home() {
   );
 }
 
-const Ul = styled("ul", {
-  listStyleType: "circle",
-  listStylePosition: "outside",
-});
+const Hr = styled("hr", {});
 
 import useTranslation from "next-translate/useTranslation";
 
@@ -49,7 +84,8 @@ import Container from "@/components/Container";
 import TransWithComps from "@/components/i18nStuff/TransWithComps";
 import HeroBand from "@/components/HeroBand";
 import Projects from "@/components/ProjectStuff/Projects";
-import { FABContainer, Box } from "@/ui/primitives/";
+import { FABContainer, Box, Flex } from "@/ui/primitives/";
+import Text from "@/components/ui/Text";
 import BackToTop from "@/ui/BackToTop";
 import StyledLink from "@/ui/StyledLink";
 
