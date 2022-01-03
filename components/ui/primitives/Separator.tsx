13fcafbfa11391b1b56a46bc027782dacaa04577
Igor Bedesqui <igorbedesqui@gmail.com>
2022-01-03 17:10:14 -0300

    feat: allow explicit height/width on separator

--- components/ui/primitives/Separator.tsx
+++ components/ui/primitives/Separator.tsx
@@ -1,7 +1,7 @@
 const Separator = styled(SeparatorPrimitive.Root, {
   backgroundColor: "$mauve6",
-  "&[data-orientation=horizontal]": { height: 1, width: "100%" },
-  "&[data-orientation=vertical]": { height: "100%", width: 1 },
+  "&[data-orientation=horizontal]": { height: 1 },
+  "&[data-orientation=vertical]": { width: 1 },
 });
 
 export default Separator;
