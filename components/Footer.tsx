80ac6ca68d20dd05e8b2385e6563d91a2ea95477
Igor Bedesqui <igorbedesqui@gmail.com>
2022-01-05 21:21:17 -0300

    feat: updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,61 +5,119 @@ interface FooterProps {
 const Footer: React.FC<FooterProps> = ({ dark }) => {
   const { t } = usetranslation();
 
-  const linkClassList = `text-sm text-opacity-80 hover:text-opacity-100 focus:text-opacity-100 transition-all ${
-    dark ? "text-mauveDark-mauve12" : "text-mauve-mauve12"
-  }`;
-
   return (
-    <footer
-      className={`flex flex-col items-center pb-8 ${
-        dark ? "bg-mauveDark-mauve1" : "bg-mauve-mauve1"
-      }`}
+    <Box
+      css={{
+        display: "flex",
+        justifyContent: "center",
+        width: "100%",
+      }}
     >
-      <nav aria-label={t("common:secondary")}>
-        <div className="flex space-x-4 mb-4">
-          <ExternalLink
-            href="https://github.com/bdsqqq"
-            aClassList={linkClassList}
+      <Box
+        css={{
+          maxWidth: "72rem",
+          px: "$spacing-08",
+          width: "100%",
+        }}
+        className={dark ? darkTheme : ""}
+      >
+        <Separator
+          css={{
+            width: "45px",
+            marginY: "$spacing-07",
+          }}
+        />
+        <Box as="footer" css={{ paddingBottom: "$spacing-10" }}>
+          <Grid
+            css={{
+              display: "grid",
+              rowGap: "$spacing-07",
+              columnGap: "$spacing-03",
+
+              gridTemplateColumns: "repeat(2, 1fr)",
+
+              "@md": {
+                gridTemplateColumns: "repeat(3, 1fr)",
+              },
+
+              "@lg": {
+                gridTemplateColumns: "repeat(4, 1fr)",
+              },
+            }}
           >
-            <span className="sr-only">Github</span>
-            <svg className="h-5 w-5" viewBox="0 0 24 24">
-              <g
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-              </g>
-            </svg>
-          </ExternalLink>
-          <ExternalLink
-            href="mailto:igorbedesqui@gmail.com"
-            aClassList={linkClassList}
-          >
-            <span className="sr-only">Email</span>
-            <svg className="h-5 w-5" viewBox="0 0 24 24">
-              <g
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
-                <polyline points="22,6 12,13 2,6" />
-              </g>
-            </svg>
-          </ExternalLink>
-        </div>
-      </nav>
-    </footer>
+            <Box
+              css={{
+                gridColumn: "1 / -1",
+
+                "@lg": {
+                  gridColumn: "auto",
+                },
+              }}
+            >
+              <StyledLink href="/">
+                <Text as="a" bold>
+                  IgorBedesqui
+                </Text>
+              </StyledLink>
+            </Box>
+
+            {/* <LinkList>
+            <Title as="h6">Me</Title>
+            <li>About</li>
+            <li>Now</li>
+          </LinkList>
+
+          <LinkList>
+            <Title as="h6">Work</Title>
+            <li>Projects</li>
+            <li>Resume</li>
+          </LinkList> */}
+
+            <LinkList>
+              <Title as="h6">Connect</Title>
+              <li>
+                <StyledLink bold={false} href="https://github.com/bdsqqq/">
+                  Github
+                </StyledLink>
+              </li>
+              <li>
+                <StyledLink bold={false} href="https://twitter.com/bedesqui">
+                  Twitter
+                </StyledLink>
+              </li>
+              <li>
+                <StyledLink
+                  bold={false}
+                  href="https://www.linkedin.com/in/igor-bedesqui/"
+                >
+                  Linkedin
+                </StyledLink>
+              </li>
+            </LinkList>
+          </Grid>
+        </Box>
+      </Box>
+    </Box>
   );
 };
+
+const Grid = Box;
+
+const LinkList = styled("ul", {
+  "& li": {
+    marginTop: "$spacing-03",
+  },
+});
+const Title = styled("h6", {
+  fontWeight: "bold",
+});
 
 export default Footer;
 
 import Link from "next/link";
-import ExternalLink from "./ExternalLink";
+import { darkTheme, styled } from "stitches.config";
 import usetranslation from "next-translate/useTranslation";
+
+import { Box, Separator } from "@/ui/primitives";
+import StyledLink from "@/ui/StyledLink";
+import Text from "@/ui/Text";
