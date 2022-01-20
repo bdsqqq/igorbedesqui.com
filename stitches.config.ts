2f7279f5b240710e80823bd2a2e8ff6e8da5cf6c
Igor Bedesqui <igorbedesqui@gmail.com>
2022-01-19 23:33:42 -0300

    fix: added missing unit

--- stitches.config.ts
+++ stitches.config.ts
@@ -19,7 +19,7 @@ export const {
     },
     fontSizes: {
       xs: "0.75rem", // 12px
-      sm: "0.875", // 14px
+      sm: "0.875rem", // 14px
       base: "1rem", // 16px
       lg: "1.25rem", // 20px
       xl: "1.5rem", // 24px
