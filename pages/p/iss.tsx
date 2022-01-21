4063a25a9eaf78b403d0b14455b613e720c08d74
Igor Bedesqui <igorbedesqui@gmail.com>
2022-01-21 17:21:43 -0300

    feat: linked to upstash

--- pages/p/iss.tsx
+++ pages/p/iss.tsx
@@ -48,7 +48,11 @@ export default function Iss() {
               Request limitations
             </Text>
             <Text>
-              {`By using data from a third party, I had a couple of limitations, the most important of them was that I could only make one request per second to the API across all clients, meaning that if enough people visited the website at the same time, I would quickly exceed my API quota. This called for an interesting solution; creating a serverless cache layer to store fresh data and distribute it or refresh it when apropriate is something I never did before but had fun implementing. Using a blazing fast cache like redis on Upstash(coloca o link do upstash aqui igor do futuro) allowed me to deliver an almost instant experience to users while keeping the amount of requests to an external API in check.`}
+              {`By using data from a third party, I had a couple of limitations, the most important of them was that I could only make one request per second to the API across all clients, meaning that if enough people visited the website at the same time, I would quickly exceed my API quota. This called for an interesting solution; creating a serverless cache layer to store fresh data and distribute it or refresh it when apropriate is something I never did before but had fun implementing. Using a blazing fast cache like redis on`}{" "}
+              <StyledLinkWithIcon href="https://upstash.com/">
+                Upstash
+              </StyledLinkWithIcon>
+              {` allowed me to deliver an almost instant experience to users while keeping the amount of requests to an external API in check.`}
               <br />
               <br />
               {`While server-side challenges are something I don't deal with that much, this one proved to be an interesting learning experience and tought me a pattern that I'm sure I'll use on the future. Also, if you're a nerd, here's a diagram of the cache logic. Simple but effective stuff.`}
@@ -194,8 +198,6 @@ export default function Iss() {
     </>
   );
 }
-
-const Figure = styled(Box, {});
 
 import useTranslation from "next-translate/useTranslation";
 import TransWithComps from "@/components/i18nStuff/TransWithComps";
