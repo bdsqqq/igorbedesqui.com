c845040a1197e3bd8b4d44aa5dfdc4ca505b532b
Igor Bedesqui <igorbedesqui@gmail.com>
2022-02-09 22:54:28 -0300

    feat: added autocomplete to translation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 export default function Home() {
-  const { t, lang } = useTranslation("home");
+  const homeTranslation = useTranslation("home");
+  const t = (translationKey: Leaves<typeof homeNamespace>) => {
+    return homeTranslation.t(translationKey);
+  };
+
   const wasmGifMeta = useMeta("wasmGif", "projs");
   const bebopMeta = useMeta("bebop", "projs");
   const projsMeta = [bebopMeta, wasmGifMeta];
 
   return (
     <>
-      <Seo t={t} lang={lang} />
+      <Seo t={homeTranslation.t} lang={homeTranslation.lang} />
 
       <Container key="index">
         <HeroBand>
           <TransWithComps
-            i18nKey="home:hero"
+            text={t("hero")}
             extraComponents={{
               intro: <span className="font-light text-lg md:text-2xl" />,
             }}
@@ -19,7 +23,7 @@ export default function Home() {
         </HeroBand>
         <Band headline={{ bold: "01", thin: t("01Title") }}>
           <p className="text-xl md:text-3xl tracking-tight ">
-            <TransWithComps i18nKey="home:01Copy" />
+            <TransWithComps text={t("01Copy")} />
           </p>
         </Band>
         <Band dark headline={{ bold: "02", thin: t("02Title") }}>
@@ -28,7 +32,7 @@ export default function Home() {
         <Band headline={{ bold: t("hey"), thin: t("heyTitle") }}>
           <p className="text-2xl">
             <TransWithComps
-              i18nKey="home:heyCopy"
+              text={t("heyCopy")}
               extraComponents={{
                 github: (
                   <StyledLink href="https://github.com/bdsqqq"></StyledLink>
@@ -60,3 +64,6 @@ import Projects from "@/components/ProjectStuff/Projects";
 import { FABContainer } from "@/ui/primitives/";
 import BackToTop from "@/ui/BackToTop";
 import StyledLink from "@/ui/StyledLink";
+
+import type { Leaves } from "@/lib/nestedKeyOfTypes";
+import homeNamespace from "@/locales/en/home.json";
