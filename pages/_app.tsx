44c8571de4a86defa4c85caa8a4f48fc0391234f
Igor Bedesqui <igorbedesqui@gmail.com>
2022-02-14 21:40:18 -0300

    feat: added css reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ const globalStyles = globalCss({
       "U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,U+FEFF, U+FFFD",
   },
 
-  "*": {
+  "*, *::before, *::after": {
     padding: 0,
     margin: 0,
     boxSizing: "border-box",
@@ -23,6 +23,9 @@ const globalStyles = globalCss({
 
   "html, body": {
     backgroundColor: "$mauve1",
+    height: "100%",
+
+    fontFamily: "Mplus, Sans-serif",
   },
 
   html: {
@@ -31,9 +34,43 @@ const globalStyles = globalCss({
     fontSmooth: "auto",
   },
 
+  body: {
+    lineHeight: "1.5",
+    fontSmooth: "always",
+  },
+
+  "img, picture, video, canvas, svg": {
+    display: "block",
+    maxWidth: "100%",
+  },
+
   "::selection": {
     backgroundColor: "$mauve12",
     color: "$mauve1",
+  },
+
+  "input, button, textarea, select": {
+    font: "inherit",
+  },
+
+  "p, h1, h2, h3, h4, h5, h6": {
+    overflowWrap: "break-word",
+  },
+
+  "h1, h2, h3, h4, h5, h6": {
+    fontSize: "inherit",
+    fontWeight: "inherit",
+  },
+
+  a: {
+    textDecoration: "none",
+    color: "inherit",
+  },
+
+  "ol, ul": {
+    listStyle: "none",
+    margin: 0,
+    padding: 0,
   },
 
   /* Remove Safari input shadow on mobile */
@@ -44,6 +81,7 @@ const globalStyles = globalCss({
     appearance: "none",
   },
 });
+
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
   return (
