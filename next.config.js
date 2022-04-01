acc51e660b14fbbf6b1f93116371dad5b694bd71
Igor Bedesqui <igorbedesqui@gmail.com>
2022-03-31 23:51:27 -0300

    fix: preact was breaking Popover Component

--- next.config.js
+++ next.config.js
@@ -10,12 +10,12 @@ module.exports = nextTranslate({
 
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      // IGOR, THIS WILL PROBABLY BREAK AT SOME POINT, IT IS A WORKAROUND, USUALLY jsx-runtime DOESN'T NEED THE .js
-      config.resolve.alias["react/jsx-runtime.js"] = "preact/jsx-runtime";
-      config.resolve.alias["react"] = "preact/compat";
-      config.resolve.alias["react-dom"] = "preact/compat";
-    }
+    // if (!dev && !isServer) {
+    //   // IGOR, THIS WILL PROBABLY BREAK AT SOME POINT, IT IS A WORKAROUND, USUALLY jsx-runtime DOESN'T NEED THE .js
+    //   config.resolve.alias["react/jsx-runtime.js"] = "preact/jsx-runtime";
+    //   config.resolve.alias["react"] = "preact/compat";
+    //   config.resolve.alias["react-dom"] = "preact/compat";
+    // }
 
     return config;
   },
