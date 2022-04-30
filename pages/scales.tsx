4c495389241aee2d18a2af8a7909b78550d3b9dc
Igor Bedesqui <igorbedesqui@gmail.com>
2022-04-29 21:41:44 -0300

    feat: added areakit Combobox logic to ratio

--- pages/scales.tsx
+++ pages/scales.tsx
@@ -1,13 +1,87 @@
 // This is based in the article by spencer mortensen at https://spencermortensen.com/articles/typographic-scale/
 
+const preDefinedScales = [
+  {
+    name: "1.067 – Minor Second",
+    value: "1.067",
+  },
+  {
+    name: "1.125 – Major Second",
+    value: "1.125",
+  },
+  {
+    name: "1.200 – Minor Third",
+    value: "1.200",
+  },
+  {
+    name: "1.250 – Major Third",
+    value: "1.250",
+  },
+  {
+    name: "1.333 – Perfect Fourth",
+    value: "1.333",
+  },
+  {
+    name: "1.414 – Augmented Fourth",
+    value: "1.414",
+  },
+  {
+    name: "1.500 – Perfect Fifth",
+    value: "1.500",
+  },
+  {
+    name: "1.618 – Golden Ratio",
+    value: "1.618",
+  },
+];
+
 const Scales = () => {
-  const [ratio, setRatio] = useState(1.618034);
+  const [ratio, setRatio] = useState(parseFloat(preDefinedScales[0].value));
   const [scaleLength, setScaleLength] = useState(2);
   const [f0, setF0] = useState(1); // f0 == fundamental frequency. Usually this is 12pt(1 pica) for print but using 1(rem) makes sense since it's equivalent to 16px.
 
   const getFi = (i: number, ratio: number, scaleLength: number, f0: number) => {
     return f0 * Math.pow(ratio, i / scaleLength);
   };
+
+  const comboboxState = useComboboxState({
+    defaultValue: preDefinedScales[0].value,
+    gutter: 8,
+    sameWidth: true,
+  });
+
+  const [filteredPreDefinedScales, setFilteredPreDefinedScales] =
+    useState(preDefinedScales);
+
+  useEffect(() => {
+    const valueWithoutComma = comboboxState.value.replace(",", ".");
+    if (!isNaN(parseFloat(valueWithoutComma))) {
+      setRatio(parseFloat(valueWithoutComma));
+    } else {
+      setRatio(2);
+    }
+  }, [comboboxState.value]);
+
+  useEffect(() => {
+    setFilteredPreDefinedScales(
+      preDefinedScales.filter((scale) => {
+        return (
+          scale.name
+            .toLowerCase()
+            .replace(".", "")
+            .includes(comboboxState.value.toLowerCase()) ||
+          scale.value
+            .toLowerCase()
+            .replace(".", "")
+            .includes(comboboxState.value.toLowerCase().replace(",", "")) ||
+          scale.name
+            .toLowerCase()
+            .includes(comboboxState.value.toLowerCase()) ||
+          scale.value.toLowerCase().includes(comboboxState.value.toLowerCase())
+        );
+      })
+    );
+  }, [comboboxState.value]);
 
   // I want this page to:
   // - talk a bit about how scales
@@ -25,15 +99,26 @@ const Scales = () => {
         <form style={{ display: "flex", flexDirection: "column" }}>
           <label htmlFor="ratio">
             Ratio:
-            <input
-              name="ratio"
-              type="number"
-              value={ratio}
-              onChange={(event) => {
-                setRatio(parseFloat(event.target.value));
-              }}
+            <Combobox
+              autoComplete="list"
+              autoSelect
+              state={comboboxState}
+              placeholder="e.g., Golden ratio"
             />
           </label>
+          {filteredPreDefinedScales.length > 0 && (
+            <ComboboxPopover state={comboboxState} className="popover">
+              {filteredPreDefinedScales.map((scale) => (
+                <ComboboxItem
+                  key={`${scale.name}_ComboboxItem`}
+                  className="combobox-item"
+                  value={scale.value}
+                >
+                  {scale.name}
+                </ComboboxItem>
+              ))}
+            </ComboboxPopover>
+          )}
 
           <label htmlFor="scaleLength">
             Scale Length:
@@ -67,6 +152,14 @@ const Scales = () => {
 
 export default Scales;
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {
+  Combobox,
+  ComboboxItem,
+  ComboboxPopover,
+  useComboboxState,
+} from "ariakit/combobox";
+import { styled } from "stitches.config";
+
 import Band from "@/components/Band";
 import Container from "@/components/Container";
