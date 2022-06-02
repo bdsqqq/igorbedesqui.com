0363fb0da4035d0ebec71d103aa20cad654c6711
Igor Bedesqui <igorbedesqui@gmail.com>
2022-06-02 13:04:21 -0300

    build: remove unused webpack config, added typing, add swcMinify flag.
    see: https://github.com/TheoBr/init.tips/pull/7/commits/ff7c28ccc498134a4461811148336a8b3ff7a38d#diff-882b2c04b01b2e8b2cdcf1817c30ea503a8005f1c0d54cfff37053b6801fea85

--- next.config.js
+++ next.config.js
@@ -1,22 +1,12 @@
+/** @type {import('next').NextConfig} */
 const nextTranslate = require("next-translate");
 // if you ever need to add more configs, check out the documentation for next-translate on github
 
 module.exports = nextTranslate({
+  swcMinify: true,
   reactStrictMode: true,
   i18n: {
     locales: ["en", "pt"],
     defaultLocale: "en",
-  },
-
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    // if (!dev && !isServer) {
-    //   // IGOR, THIS WILL PROBABLY BREAK AT SOME POINT, IT IS A WORKAROUND, USUALLY jsx-runtime DOESN'T NEED THE .js
-    //   config.resolve.alias["react/jsx-runtime.js"] = "preact/jsx-runtime";
-    //   config.resolve.alias["react"] = "preact/compat";
-    //   config.resolve.alias["react-dom"] = "preact/compat";
-    // }
-
-    return config;
   },
 });
