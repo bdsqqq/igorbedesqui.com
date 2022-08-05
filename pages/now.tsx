ee525434b4badf24e588a32976885243905a1ee6
Igor Bedesqui <igorbedesqui@gmail.com>
2022-08-05 15:47:54 -0300

    feat: update now page layout

--- pages/now.tsx
+++ pages/now.tsx
@@ -14,21 +14,38 @@ export default function Home() {
 
       <Container backable key="index">
         <Band gridless id="main">
-          <Box css={{ maxWidth: "60ch", marginX: "auto" }}>
+          <Box css={{ marginX: "auto" }}>
             <Text as="h1" presetStyle="heading">
               {t("doingTitle")}
             </Text>
+            <Text
+              as="p"
+              css={{
+                color: "$mauve11",
+                fontSize: "$sm",
+                fontWeight: "600",
+              }}
+            >
+              <TransWithComps
+                text={t("lastUpdated")}
+                extraComponents={{ date: <span /> }}
+              />
+            </Text>
+
+            <Box
+              css={{
+                height: "$space$spacing-10",
+              }}
+            />
+
             <Flex
               as="ul"
               css={{
                 flexDirection: "column",
                 gap: "$spacing-03",
 
-                marginTop: "$spacing-06",
-                marginBottom: "$spacing-11",
-
                 listStyleType: "circle",
-                listStylePosition: "outside",
+                listStylePosition: "inside",
               }}
             >
               {thingsImDoing.map((thing) => {
@@ -36,7 +53,7 @@ export default function Home() {
                   <Text
                     as="li"
                     css={{
-                      fontSize: "$lg",
+                      fontSize: "$md",
                       lineHeight: "$lg",
                     }}
                     key={thing}
@@ -46,34 +63,8 @@ export default function Home() {
                 );
               })}
             </Flex>
-
-            <Text
-              as="p"
-              css={{
-                marginBottom: "$spacing-07",
-              }}
-            >
-              <TransWithComps text={t("bottomText")} />
-            </Text>
-
-            <Separator
-              css={{
-                width: "12rem",
-                marginX: "auto",
-                marginBottom: "$spacing-07",
-                borderColor: "$mauve6",
-
-                "@md": {
-                  width: "24rem",
-                },
-              }}
-            />
-
             <Text as="p">
-              <TransWithComps
-                text={t("lastUpdated")}
-                extraComponents={{ date: <span /> }}
-              />
+              <TransWithComps text={t("bottomText")} />
             </Text>
           </Box>
         </Band>
@@ -87,12 +78,5 @@ import Seo from "@/components/Seo";
 import Band from "@/components/Band";
 import Container from "@/components/Container";
 import TransWithComps from "@/components/i18nStuff/TransWithComps";
-import HeroBand from "@/components/HeroBand";
-import Projects from "@/components/ProjectStuff/Projects";
-import { FABContainer, Box, Flex, Separator } from "@/ui/primitives/";
+import { Box, Flex } from "@/ui/primitives/";
 import Text from "@/components/ui/Text";
-import BackToTop from "@/ui/BackToTop";
-import StyledLink from "@/ui/StyledLink";
-
-import { styled } from "stitches.config";
-import { useEffect } from "react";
