9e560e211914cc5f63fadbece9c3df19b2bfa7a1
Igor Bedesqui <igorbedesqui@gmail.com>
2022-10-14 16:06:23 -0300

    feat: add animation tokens to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,41 @@ module.exports = {
   ],
   theme: {
     extend: {
+      transitionTimingFunction: {
+        /**
+         * See: https://carbondesignsystem.com/guidelines/motion/overview/
+         */
+        "productive-standard": "cubic-bezier(0.2, 0, 0.38, 0.9)",
+        "productive-entrance": "cubic-bezier(0, 0, 0.38, 0.9)",
+        "productive-exit": "cubic-bezier(0.2, 0, 1, 0.9)",
+        "expressive-standard": "cubic-bezier(0.4, 0.14, 0.3, 1)",
+        "expressive-entrance": "cubic-bezier(0, 0, 0.3, 1)",
+        "expressive-exit": "cubic-bezier(0.4, 0.14, 1, 1)",
+      },
+
+      transitionDuration: {
+        /**
+         * fast-01 - 70ms - Micro-interactions such as button and toggle
+         *
+         * fast-02 - 110ms - Micro-interactions such as fade
+         *
+         * moderate-01 - 150ms - Micro-interactions, small expansion, short distance movements
+         *
+         * moderate-02 - 240ms - Expansion, system communication, toast
+         *
+         * slow-01 - 400ms - Large expansion, important system notifications
+         *
+         * slow-02 - 700ms - Background dimming
+         *
+         * See: https://carbondesignsystem.com/guidelines/motion/overview/
+         */
+        "fast-01": "70ms",
+        "fast-02": "110ms",
+        "moderate-01": "150ms",
+        "moderate-02": "240ms",
+        "slow-01": "400ms",
+        "slow-02": "700ms",
+      },
       colors: {
         ...mauveDark,
         ...crimsonDark,
