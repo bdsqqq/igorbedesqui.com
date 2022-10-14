f81f3671bde6ba7e1e7cd0cb1863354a5170dbb0
Igor Bedesqui <igorbedesqui@gmail.com>
2022-10-14 15:41:37 -0300

    feat: add theme colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const { mauveDark, crimsonDark } = require("@radix-ui/colors");
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...mauveDark,
+        ...crimsonDark,
+      },
+    },
   },
   plugins: [],
 };
