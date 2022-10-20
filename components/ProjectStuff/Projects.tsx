5201a332672af5138a5aa6777dcf959d3f8eca04
Igor Bedesqui <igorbedesqui@gmail.com>
2022-10-20 00:08:57 -0300

    feat: tailwindify Projects list

--- components/ProjectStuff/Projects.tsx
+++ components/ProjectStuff/Projects.tsx
@@ -3,108 +3,28 @@ type ProjectsProps = {
 };
 const Projects: React.FC<ProjectsProps> = ({ projectsMeta }) => {
   return (
-    <ProjsList>
+    <ul className="flex flex-wrap flex-col items-start justify-start w-full pointer-events-none md:grid md:grid-cols-2 md:gap-x-6 hover:text-mauve11 [&_>_a]:hover:opacity-40">
       {projectsMeta.map((project, i) => {
         return (
-          <Link href={`/p/${project.urlSlug}`} key={project.name}>
+          <UnstyledLink
+            className="w-full py-8 pointer-events-auto first:pt-0 last:pb-0 md:py-6 md:[&:nth-child(2)]:pt-0 md:[&:nth-last-child(2)_:nth-child(odd)]:pb-0 hover:!text-mauve12 hover:!opacity-100 motion-safe:transition-all duration-moderate-01 ease-productive-standard"
+            href={`/p/${project.urlSlug}`}
+            key={project.name}
+          >
             <li>
-              <Box
-                as="h3"
-                css={{
-                  fontSize: "$lg",
-                  fontWeight: "600",
-                  letterSpacing: "$tighter",
-                }}
-              >
+              <h3 className="text-xl font-semibold tracking-tighter">
                 {project.name}
-              </Box>
+              </h3>
               <p>{project.description}</p>
             </li>
-          </Link>
+          </UnstyledLink>
         );
       })}
-    </ProjsList>
+    </ul>
   );
 };
 
-const ProjsList = styled("ul", {
-  width: "100%",
-
-  display: "flex",
-  flexWrap: "wrap",
-  flexDirection: "column",
-  alignItems: "start",
-  justifyContent: "start",
-
-  pointerEvents: "none",
-  "&:hover": {
-    color: "$mauve11",
-
-    "& > a": {
-      opacity: "0.4",
-    },
-  },
-
-  "@md": {
-    display: "grid",
-    gridTemplateColumns: "repeat(2, 1fr)",
-    columnGap: "$spacing-06",
-  },
-});
-
-const Link = styled(UnstyledLink, {
-  width: "100%",
-  py: "$spacing-07",
-
-  // this pointer event will bubble up and activate the hover style of the parent
-  pointerEvents: "auto",
-
-  "&:first-child": {
-    paddingTop: 0,
-  },
-  "&:last-child": {
-    paddingBottom: 0,
-  },
-
-  "@md": {
-    py: "$spacing-06",
-
-    "&:nth-child(2)": {
-      paddingTop: 0,
-    },
-
-    // remove padding bottom of second last child only if it is in the last line eg:
-    /* 
-      | 1 | 2 |
-      | 3 |
-      > 2 is the second last but it still keeps the padding bottom
-
-      | 1 | 2 |
-      | 3 | 4 |
-      > 3 is the second last and in the last row so it loses it's padding bottom
-
-      btw, this 2 is tied to the gridTemplateColumns repeat(2, 1fr) of the parent. If I change that, then I'll have to update this part but it definetely isn't worth abstracting it rn
-    */
-    "&:nth-last-child(2):nth-child(odd)": {
-      paddingBottom: 0,
-    },
-  },
-
-  "&:hover": {
-    color: "$mauve12",
-    opacity: "1 !important",
-  },
-
-  "@motionOk": {
-    transitionProperty: "color opacity",
-    transitionDuration: duration.moderate01,
-    transitionTimingFunction: timingFunction.productive.standard,
-  },
-});
-
 export default Projects;
 
-import { styled } from "stitches.config";
-import { Box, UnstyledLink } from "@/ui/primitives";
+import { UnstyledLink } from "@/ui/primitives";
 import { Meta } from "@/hooks/useMeta";
-import { duration, timingFunction } from "@/animations";
