abd995fdb820b2f225bec99a998b8c0788f4f391
Igor Bedesqui <igorbedesqui@gmail.com>
2022-10-25 23:25:10 -0300

    refactor: remove unused import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { HistoryProvider } from "@/contexts/History";
-import { styled } from "stitches.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
