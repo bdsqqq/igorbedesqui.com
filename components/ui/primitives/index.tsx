6a4b4e33b9e430c3e8f52d8ee684c1e337274784
Igor Bedesqui <igorbedesqui@gmail.com>
2022-10-25 23:20:03 -0300

    refactor: remove SrOnly component

--- components/ui/primitives/index.tsx
+++ components/ui/primitives/index.tsx
@@ -1,8 +1,7 @@
 import { default as Box } from "./Box";
 import { default as Span } from "./Span";
 import { default as FABContainer } from "./FABContainer";
-import { default as SrOnly } from "./SrOnly";
 import { default as UnstyledLink } from "./UnstyledLink";
 import { default as Separator } from "./Separator";
 
-export { Box, Span, FABContainer, SrOnly, UnstyledLink, Separator };
+export { Box, Span, FABContainer, UnstyledLink, Separator };
