883b8a128e1e843bde8fc78e2df8cd49f566c5b7
Igor Bedesqui <igorbedesqui@gmail.com>
2022-10-28 18:51:07 -0300

    refactor: delete unused styled primitives

--- components/ui/primitives/Span.tsx
+++
@@ -1,5 +0,0 @@
-const Span = styled("span");
-
-export default Span;
-
-import { styled } from "stitches.config";
--- components/ui/primitives/index.tsx
+++ components/ui/primitives/index.tsx
@@ -1,7 +1,5 @@
-import { default as Box } from "./Box";
-import { default as Span } from "./Span";
 import { default as FABContainer } from "./FABContainer";
 import { default as UnstyledLink } from "./UnstyledLink";
 import { default as Separator } from "./Separator";
 
-export { Box, Span, FABContainer, UnstyledLink, Separator };
+export { FABContainer, UnstyledLink, Separator };
