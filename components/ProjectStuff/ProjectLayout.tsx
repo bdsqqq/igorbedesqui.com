27dcc42f96d9f0546ed19587943b7cf08eca6268
Igor Bedesqui <igorbedesqui@gmail.com>
2022-10-31 23:29:10 -0300

    feat: un-i18nize ProjectLayout

--- components/ProjectStuff/ProjectLayout.tsx
+++ components/ProjectStuff/ProjectLayout.tsx
@@ -2,8 +2,6 @@ export const ProjectLayout: React.FC<{
   projMeta: Meta;
   nextProjMeta?: Meta;
 }> = ({ children, projMeta, nextProjMeta }) => {
-  const { t } = useTypeSafeTranslation("detail");
-
   return (
     <div className="grid md:gap-5 md:[grid-template-columns:1fr_220px] lg:gap-20 lg:[grid-template-columns:1fr_330px] px-8 md:px-16">
       <div>{children}</div>
@@ -12,7 +10,7 @@ export const ProjectLayout: React.FC<{
         <div className="sticky top-[1.5rem] pt-8 px-8 md:pr-24 md:px-0 md:right-16 overflow-auto max-h-[calc(100vh_-_1.5rem)]">
           <div>
             <div className="mb-6">
-              <p className="font-semibold">{t("role")}</p>
+              <p className="font-semibold">Type of work</p>
               <ul>
                 {projMeta.roles.map((role, i) => {
                   return (
@@ -26,7 +24,7 @@ export const ProjectLayout: React.FC<{
             </div>
 
             <div className="mb-6">
-              <p className="font-semibold">{t("tools")}</p>
+              <p className="font-semibold">Tools</p>
               <ul>
                 {projMeta.tools.map((tool, i) => {
                   return (
@@ -40,12 +38,12 @@ export const ProjectLayout: React.FC<{
             </div>
 
             <div className="mb-8">
-              <span className="font-semibold">{t("date")}</span>
+              <span className="font-semibold">Date</span>
               <p>{projMeta.date}</p>
             </div>
 
             <div className="mb-6">
-              <span className="font-semibold">{t("type")}</span>
+              <span className="font-semibold">Project type</span>
               <p>{projMeta.type}</p>
             </div>
 
@@ -54,7 +52,7 @@ export const ProjectLayout: React.FC<{
                 <Separator className="w-11 my-12" />
 
                 <div className="mb-6">
-                  <span className="font-semibold">{t("next")}</span>
+                  <span className="font-semibold">Next project</span>
                   <span className="flex gap-0.5 items-center text-mauve11">
                     <StyledLink scroll href={`/p/${nextProjMeta.urlSlug}`}>
                       {nextProjMeta.name}
@@ -74,4 +72,3 @@ import { Separator } from "@/ui/primitives/";
 import StyledLink from "@/ui/StyledLink";
 
 import { Meta } from "@/hooks/useMeta";
-import { useTypeSafeTranslation } from "@/hooks/useTypeSafeTranslation";
