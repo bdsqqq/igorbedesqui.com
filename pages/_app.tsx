6eeb19915dcbbf3d889df88154b35864903281af
Igor Bedesqui <igorbedesqui@gmail.com>
2022-11-11 17:25:11 -0300

    feat: bg closer to black, text less white

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
           </div>
           <div
-            className="w-full h-full top-0 absolute opacity-[2%] z-[3] filter blur-[100px]"
+            className="w-full h-full top-0 absolute opacity-[2%] z-[3] filter blur-[100px] saturate-150"
             style={{
               backgroundImage:
                 "radial-gradient(at 27% 37%,#3a8bfd 0,transparent 0),radial-gradient(at 97% 21%,#72fe7d 0,transparent 50%),radial-gradient(at 52% 99%,#fd3a4e 0,transparent 50%),radial-gradient(at 10% 29%,#855afc 0,transparent 50%),radial-gradient(at 97% 96%,#e4c795 0,transparent 50%),radial-gradient(at 33% 50%,#8ca8e8 0,transparent 50%),radial-gradient(at 79% 53%,#eea5ba 0,transparent 50%)",
