fbef2a2cbcb51d4aad567e28ae0ee997aeee95d3
Igor Bedesqui <igorbedesqui@gmail.com>
2022-11-14 17:35:08 -0300

    feat: grain at z-40, gradient at z-30

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,17 +19,15 @@ function MyApp({ Component, pageProps }: AppProps) {
         }}
       >
         <div className={`${customFont.className} relative`}>
-          <div className="pointer-events-none absolute inset-0 z-50 w-full h-full bg-[url('/images/grain.png')] bg-left-top bg-[length:250px] opacity-25" />
-          <div className="relative z-10">
-            <Component {...pageProps} />
-          </div>
+          <div className="z-40 pointer-events-none absolute inset-0 w-full h-full bg-[url('/images/grain.png')] bg-left-top bg-[length:250px] opacity-25" />
           <div
-            className="pointer-events-none w-full h-full top-0 absolute opacity-[5%] z-[50] filter blur-[100px] saturate-150"
+            className="z-[30] pointer-events-none w-full h-full top-0 absolute opacity-[5%] filter blur-[100px] saturate-150"
             style={{
               backgroundImage:
                 "radial-gradient(at 27% 37%,#3a8bfd 0,transparent 0),radial-gradient(at 97% 21%,#72fe7d 0,transparent 50%),radial-gradient(at 52% 99%,#fd3a4e 0,transparent 50%),radial-gradient(at 10% 29%,#855afc 0,transparent 50%),radial-gradient(at 97% 96%,#e4c795 0,transparent 50%),radial-gradient(at 33% 50%,#8ca8e8 0,transparent 50%),radial-gradient(at 79% 53%,#eea5ba 0,transparent 50%)",
             }}
           />
+          <Component {...pageProps} />
         </div>
       </MDXProvider>
     </HistoryProvider>
