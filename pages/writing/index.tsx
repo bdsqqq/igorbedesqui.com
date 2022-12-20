39082feb0e056c8a929a0fe4c2f240df8df00b57
Igor Bedesqui <igorbedesqui@gmail.com>
2022-12-20 23:57:28 +0000

    feat: basic /writing page

---
+++ pages/writing/index.tsx
@@ -0,0 +1,40 @@
+const metas = [onWritingMeta, onWritingMeta, onWritingMeta, onWritingMeta];
+
+export default function Writing() {
+  return (
+    <>
+      <Seo title="Writing" description="" url="writing" />
+
+      <Container backable key="index">
+        <Band gridless id="main">
+          <div className="mx-auto grid grid-cols-4">
+            <h1 className="text-4xl">Writing</h1>
+
+            <ul className="col-span-3 flex flex-col gap-6">
+              {metas.map((meta) => {
+                return (
+                  <UnstyledLink
+                    href={`/writing/${meta.urlSlug}`}
+                    key={meta.name}
+                  >
+                    <li>
+                      <h2 className="text-2xl">{meta.name}</h2>
+                      <p>{meta.description}</p>
+                    </li>
+                  </UnstyledLink>
+                );
+              })}
+            </ul>
+          </div>
+        </Band>
+      </Container>
+    </>
+  );
+}
+
+import Seo from "@/components/Seo";
+import Band from "@/components/Band";
+import Container from "@/components/Container";
+
+import { meta as onWritingMeta } from "./on-writing";
+import { UnstyledLink } from "@/components/ui/primitives";
