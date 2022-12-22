3f85e818497e59f054e8b60bdaa267d053196857
Igor Bedesqui <igorbedesqui@gmail.com>
2022-12-22 21:35:37 +0000

    feat: use white as hover and focus instead of gray-12

--- components/ui/StyledLink.tsx
+++ components/ui/StyledLink.tsx
@@ -30,7 +30,7 @@ const StyledLinkWithIcon: FC<StyledLinkProps & HtmlHTMLAttributes<{}>> = ({
     <UnstyledLink
       className={cx(
         cva(
-          "cursor-pointer underline underline-offset-2 motion-safe:transition-colors motion-safe:duration-moderate-01 motion-safe:ease-productive-standard hover:text-gray-12 focus-within:text-gray-12 [&>svg]:inline",
+          "cursor-pointer underline underline-offset-2 motion-safe:transition-colors motion-safe:duration-moderate-01 motion-safe:ease-productive-standard hover:text-white focus-within:text-white [&>svg]:inline",
           {
             variants: {
               bold: {
