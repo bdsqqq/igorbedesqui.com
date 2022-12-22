d05cff66379f998d10c1355c566d72531d556358
Igor Bedesqui <igorbedesqui@gmail.com>
2022-12-22 19:39:16 +0000

    feat: add work + writing

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import { meta as bebopMeta } from "data/work/bebop.mdx";
 import { meta as issMeta } from "data/work/iss.mdx";
 import { meta as wasmGifMeta } from "data/work/wasmgif.mdx";
 
+import { meta as onWrittingMeta } from "./writing/on-writing";
+
 export default function Home() {
   const projsMeta = [bebopMeta, issMeta, wasmGifMeta];
+
+  // Me, What I've done
+  // Work
+  // Writing
+  // Now / connect
 
   return (
     <>
@@ -13,49 +20,52 @@ export default function Home() {
       />
 
       <Container key="index">
-        <Band gridless id="hero">
-          <div className="grid grid-cols-4 gap-2">
-            <h1>Igor Bedesqui</h1>
-            <div className="col-span-2">
-              <p>
-                Crafting solutions. Building web experiences with care.
-                Exploring design, UX, and interactivity.
-              </p>
-              <p>Previously at IBM</p>
+        <div className="max-w-5xl mx-auto">
+          <Band gridless id="hero">
+            <div className="sm:grid grid-cols-4 flex flex-col gap-2">
+              <h1>Igor Bedesqui</h1>
+              <div className="md:col-span-2 col-span-3">
+                <p>
+                  Crafting solutions. Building web experiences with care.
+                  Exploring design, UX, and interactivity.
+                </p>
+                <p>Previously at IBM</p>
+              </div>
             </div>
-          </div>
-        </Band>
-        <Band headline={{ bold: "01", thin: "Who am I?" }}>
-          <p className="text-xl md:text-2xl">
-            In a few words:
-            <br />
-            <br />
-            I’m passionate about design, UX and interactivity. Professionally I
-            create <strong>simple</strong>, <strong>functional</strong>, and{" "}
-            <strong>beautiful</strong> solutions.
-            <br />
-            <br />
-            For kinda up to date info you can see what I’m doing{" "}
-            <StyledLink href="/now">now</StyledLink>.
-          </p>
-        </Band>
-        <Band headline={{ bold: "02", thin: "Work" }}>
-          <Projects projectsMeta={projsMeta} />
-        </Band>
-        <Band headline={{ bold: "hey", thin: "Let's connect!" }}>
-          <p className="text-xl md:text-2xl">
-            You can find me on various platforms that I don’t really like using,
-            see my code at{" "}
-            <StyledLink href="https://github.com/bdsqqq">Github</StyledLink> or
-            get in touch via{" "}
-            <StyledLink href="https://twitter.com/bedesqui">twitter</StyledLink>
-            .
-          </p>
-        </Band>
+          </Band>
+          <Band headline={{ bold: "01", thin: "Work" }}>
+            <div className="grid grid-cols-3">
+              <div className="col-start-1 col-end-3">
+                <Projects projectsMeta={projsMeta} />
+              </div>
+              <div>
+                <h2>Writing</h2>
+                <div>
+                  <ul>
+                    <li>
+                      <UnstyledLink href={`/writing/${onWrittingMeta.urlSlug}`}>
+                        <h3>{onWrittingMeta.name}</h3>
+                        <p>{onWrittingMeta.description}</p>
+                      </UnstyledLink>
+                    </li>
+                    <li>
+                      <UnstyledLink href={`/writing/${onWrittingMeta.urlSlug}`}>
+                        <h3>{onWrittingMeta.name}</h3>
+                        <p>{onWrittingMeta.description}</p>
+                      </UnstyledLink>
+                    </li>
+                  </ul>
+
+                  <span>
+                    <StyledLink href="/writing">All writing</StyledLink>
+                    {` Infrenquent thoughts on design and code.`}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </Band>
+        </div>
       </Container>
-      <FABContainer>
-        <BackToTop />
-      </FABContainer>
     </>
   );
 }
@@ -65,6 +75,6 @@ import Band from "@/components/Band";
 import Container from "@/components/Container";
 import HeroBand from "@/components/HeroBand";
 import Projects from "@/components/ProjectStuff/Projects";
-import { FABContainer } from "@/ui/primitives/";
+import { FABContainer, UnstyledLink } from "@/ui/primitives/";
 import BackToTop from "@/ui/BackToTop";
 import StyledLink from "@/ui/StyledLink";
