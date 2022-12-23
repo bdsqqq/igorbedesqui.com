626d7508c8bf5ac63a5180c13deb27133c5cb3da
Igor Bedesqui <igorbedesqui@gmail.com>
2022-12-23 20:24:24 +0000

    feat: extend tailwind grid to 16 cols

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,6 +42,29 @@ module.exports = {
       white: "hsl(0 0% 100%)",
     },
     extend: {
+      gridTemplateColumns: {
+        16: "repeat(16, minmax(0, 1fr))",
+      },
+      gridColumn: {
+        "span-13": "span 13 / span 13",
+        "span-14": "span 14 / span 14",
+        "span-15": "span 15 / span 15",
+        "span-16": "span 16 / span 16",
+      },
+      gridColumnStart: {
+        13: "13",
+        14: "14",
+        15: "15",
+        16: "16",
+        17: "17",
+      },
+      gridColumnEnd: {
+        13: "13",
+        14: "14",
+        15: "15",
+        16: "16",
+        17: "17",
+      },
       keyframes: {
         fade: {
           "0%": { opacity: "0" },
