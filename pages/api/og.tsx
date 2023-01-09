dadd6a2031220b90bd012c86186839389358199f
Igor Bedesqui <igorbedesqui@gmail.com>
2023-01-09 22:40:13 +0000

    feat: basic dynamic og implementation

---
+++ pages/api/og.tsx
@@ -0,0 +1,101 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+// Make sure the font exists in the specified path:
+const font = fetch(
+  new URL("../../public/fonts/IBMPlexSerif-Regular.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+export default async function handler(req: NextRequest) {
+  const fontData = await font;
+
+  try {
+    const { searchParams } = new URL(req.url);
+
+    // ?title=<title>
+    const hasTitle = searchParams.has("title");
+    const title = hasTitle
+      ? searchParams.get("title")?.slice(0, 100)
+      : "My default title";
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            display: "flex",
+            position: "relative",
+            width: "100%",
+            height: "100%",
+            fontFamily: "Plex",
+          }}
+        >
+          <div
+            style={{
+              padding: "2rem",
+              backgroundColor: "hsl(0 0% 4%)",
+              backgroundSize: "150px 150px",
+              height: "100%",
+              width: "100%",
+              display: "flex",
+              alignItems: "flex-start",
+              justifyContent: "space-between",
+              flexDirection: "column",
+              flexWrap: "nowrap",
+              color: "hsl(0 0% 62.8%)",
+            }}
+          >
+            <h2
+              style={{
+                fontSize: 18,
+              }}
+            >
+              {title}
+            </h2>
+            <h1
+              style={{
+                maxWidth: "66%",
+                color: "hsl(0 0% 93.0%)",
+                fontSize: 42,
+              }}
+            >
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Consequuntur minus quasi velit.
+            </h1>
+          </div>
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              zIndex: "40",
+              position: "absolute",
+              inset: "0",
+              opacity: "0.03",
+              background:
+                "url(https://www.igorbedesqui.com/images/framernoise.png)",
+            }}
+          ></div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: "Plex",
+            data: fontData,
+            style: "normal",
+          },
+        ],
+      }
+    );
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
+}
