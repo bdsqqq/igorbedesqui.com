c3743ebd3591adc3910604bcb89bdfc5f526229a
Igor Bedesqui <igorbedesqui@gmail.com>
2023-01-13 15:29:51 +0000

    feat: add content, layout udates

--- pages/p/ibm/index.tsx
+++ pages/p/ibm/index.tsx
@@ -15,16 +15,16 @@ export default function IBM() {
 
       <Container key="bebopProj" backable>
         <HeroBand heroVideo={"/videos/ibm/tapes"}>
-          Creating web experiences in the big blue.
+          Creating web experiences in the corporate of corporates.
         </HeroBand>
 
         <div className="mt-16 flex flex-col gap-20">
           <Band gridless id="context">
             <div className={cx(grid({ mode: "narrow" }))}>
               <p className="text-xl md:col-start-2 lg:col-start-2 col-span-5 lg:col-span-9">
-                {`From updating fiels in a CMS to leading the development of an
-              event with 121k attendees, I've had the opportunity to work on a
-              wide range of projects at IBM and collaborate with amazing folks.`}
+                {`From updating content in product pages to leading the development of an
+              online event with 121k attendees, I've had the opportunity to work on a
+              wide range of projects at IBM, collaborate with amazing folks, and lead impactful initiatives.`}
               </p>
             </div>
           </Band>
@@ -35,7 +35,7 @@ export default function IBM() {
                 "gap-y-8 hover:text-gray-10 focus-within:text-gray-10 pointer-events-none"
               )}
             >
-              <li className="col-span-4 lg:col-span-5 ">
+              <li className="col-span-4 lg:col-span-8 ">
                 <UnstyledLink
                   href="/p/ibm/think2022"
                   className="block pointer-events-auto hover:text-gray-12 focus:text-gray-12 transition-colors duration-fast-02 ease-productive-standard"
@@ -46,7 +46,7 @@ export default function IBM() {
                   <p>{`Creating the digital experience for IBM's biggest event with bleeding edge tech.`}</p>
                 </UnstyledLink>
               </li>
-              <li className="col-span-4 lg:col-span-5 ">
+              <li className="col-span-4 lg:col-span-8 ">
                 <UnstyledLink
                   href="/p/ibm/think2022"
                   className="block pointer-events-auto hover:text-gray-12 focus:text-gray-12 transition-colors duration-fast-02 ease-productive-standard"
@@ -55,12 +55,11 @@ export default function IBM() {
                     Interactive event app
                   </h2>
                   <p>
-                    Highlighting key offerings to convert interested attendees
-                    into blown away clients.
+                    {`Highlighting key offerings to convert interested attendees into blown away clients.`}
                   </p>
                 </UnstyledLink>
               </li>
-              <li className="col-span-4 lg:col-span-5 ">
+              <li className="col-span-4 lg:col-span-8">
                 <UnstyledLink
                   href="/p/ibm/think2022"
                   className="block pointer-events-auto hover:text-gray-12 focus:text-gray-12 transition-colors duration-fast-02 ease-productive-standard"
@@ -69,20 +68,64 @@ export default function IBM() {
                     QA Helper
                   </h2>
                   <p>
-                    Automating boring tasks to help engineers focus on what
-                    matters. Typesafelly, of course.
+                    {`Automating QA tasks in browser to help engineers focus on what matters. Typesafelly, of course.`}
                   </p>
                 </UnstyledLink>
+              </li>
+              <li className="pointer-events-none col-span-4 lg:col-span-8">
+                <h2 className="font-semibold">
+                  Open Source Exploration initiative
+                </h2>
+                <p>
+                  Nerding about cool tech to the point it became an actual job
+                  activity when our studio decided to focus on unique web
+                  experiences.
+                </p>
+              </li>
+              <li className="pointer-events-none col-span-6 lg:col-span-12 lg:col-end-14">
+                <h2 className="font-semibold">Other contributions</h2>
+                <ul className="list-decimal list-outside marker:text-gray-8 space-y-2">
+                  <li>
+                    Trained colleagues in React, Typescript, and NextJS through
+                    presentations, one on one sessions, and collaborative work.
+                  </li>
+
+                  <li>
+                    {`Maintained, used, and improved template repositories used to create whitepapers and other deliverables.`}
+                  </li>
+
+                  <li>
+                    {`Created pages and maintained one of a kind assets for ibm.com /industries and /services.`}
+                  </li>
+                </ul>
               </li>
             </ul>
           </Band>
           <Band gridless id="people">
-            <div className={cx(grid({ mode: "narrow" }), "gap-y-8")}>
-              <div className="col-span-6 lg:col-start-1 lg:col-span-8">
-                {`Amazing people, growth, thankful Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni error est animi atque laboriosam nesciunt adipisci laborum maiores ad fuga, quisquam consectetur vel omnis et cumque tempore commodi sunt sapiente iure voluptatibus dolorum eveniet! Impedit obcaecati fuga distinctio a eius!`}
+            <div className={cx(grid({ mode: "narrow" }), "gap-y-20")}>
+              <div className="col-span-6 lg:col-start-1 lg:col-span-8 space-y-4">
+                <p>
+                  {`I'm thankful to have met people who changed how I approach work and life to some extent.`}
+                </p>
+                <p>
+                  {`Rafael's management style allowed me to pursue quality work, and tought me the joy of ownership; My best work happened under his leadership and together we delivered experiences that set a new standard for our organization.`}
+                </p>
+                <p>
+                  {`Sebastian's mentorship taught me how to speak, how to think about design, and from observing him, how to be a better person. I feel lucky to have the opportunity to work with him, and even luckier to call him a friend.`}
+                </p>
               </div>
-              <div className="md:col-end-9 lg:col-start-5 col-span-5 lg:col-span-12">
-                {`Why I left Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.`}
+              <div className="md:col-end-9 lg:col-start-5 col-span-5 lg:col-span-12 space-y-4">
+                <p>
+                  {`Leaving was a hard decision, but after two years of aquiring new responsibilities and skills, with no change in pay, I decided to move on. A job at a company this big is transactional relationship after all.`}
+                </p>
+                <p
+                  aria-hidden
+                  className="text-gray-1 hover:text-gray-11 transition-colors duration-fast-02 ease-productive-standard group"
+                >
+                  {`…And it freaked me out that I was paid `}
+                  <span className="group-hover:text-gray-12 transition-colors duration-fast-02 ease-productive-standard">{`7K USD a year being a major contributor to high-impact projects in a multi-billion company`}</span>
+                  {`. Being a cost savings measure as a third-party offshore contractor in Brazil isn't the best for self worth.`}
+                </p>
               </div>
             </div>
           </Band>
