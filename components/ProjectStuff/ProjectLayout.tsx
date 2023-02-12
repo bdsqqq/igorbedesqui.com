6b4f60cdde09e97185bdad2239f2d3602c07662b
Igor Bedesqui <igorbedesqui@gmail.com>
2023-02-12 01:21:45 +0000

    feat: split sidebar  mobile and desktop versions

--- components/ProjectStuff/ProjectLayout.tsx
+++ components/ProjectStuff/ProjectLayout.tsx
@@ -15,47 +15,28 @@ export const ProjectLayout: React.FC<
       <div className="col-span-full md:col-start-7 lg:col-start-13 sticky top-[1.5rem] overflow-auto max-h-[calc(100vh_-_4.5rem)]">
         <Separator className="md:hidden w-11 mb-12 mt-20" />
 
-        <div
-          className={cx(subGrid()(), "gap-y-4 md:flex md:flex-col md:gap-6")}
-        >
-          <div className="col-span-2">
-            <p className="font-bold text-gray-9">Type of work</p>
-            <ul>
-              {projMeta.roles.map((role, i) => {
-                return (
-                  <li key={`role-${i}`}>
-                    {role}
-                    {i < projMeta.roles.length - 1 && ","}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+        {/* Mobile */}
+        <div className={cx(subGrid()(), "gap-y-4 md:hidden")}>
+          <SidebarContent projMeta={projMeta} />
+          {nextProjMeta && (
+            <>
+              <div className="col-span-4">
+                <Separator className="w-11 my-12" />
 
-          <div className="col-span-2">
-            <span className="font-bold text-gray-9">Project type</span>
-            <p>{projMeta.type}</p>
-          </div>
+                <span className="font-bold text-gray-9">Next project</span>
+                <span className="flex gap-0.5 items-center text-gray-11">
+                  <StyledLink href={`/work/${nextProjMeta.urlSlug}`}>
+                    {nextProjMeta.name}
+                  </StyledLink>
+                </span>
+              </div>
+            </>
+          )}
+        </div>
 
-          <div className="col-span-2">
-            <p className="font-bold text-gray-9">Tools</p>
-            <ul>
-              {projMeta.tools.map((tool, i) => {
-                return (
-                  <li className="[display:inline_list-item]" key={i}>
-                    {tool}
-                    {i < projMeta.tools.length - 1 && ", "}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-
-          <div className="col-span-2">
-            <span className="font-bold text-gray-9">Date</span>
-            <p>{projMeta.date}</p>
-          </div>
-
+        {/* Desktop */}
+        <div className={cx(subGrid()(), "hidden md:flex md:flex-col md:gap-6")}>
+          <SidebarContent projMeta={projMeta} />
           {nextProjMeta && (
             <>
               <div className="col-span-4">
@@ -73,6 +54,49 @@ export const ProjectLayout: React.FC<
         </div>
       </div>
     </div>
+  );
+};
+
+const SidebarContent = ({ projMeta }: { projMeta: Meta }) => {
+  return (
+    <>
+      <div className="col-span-2">
+        <p className="font-bold text-gray-9">Type of work</p>
+        <ul>
+          {projMeta.roles.map((role, i) => {
+            return (
+              <li key={`role-${i}`}>
+                {role}
+                {i < projMeta.roles.length - 1 && ","}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="col-span-2">
+        <span className="font-bold text-gray-9">Project type</span>
+        <p>{projMeta.type}</p>
+      </div>
+
+      <div className="col-span-2">
+        <p className="font-bold text-gray-9">Tools</p>
+        <ul>
+          {projMeta.tools.map((tool, i) => {
+            return (
+              <li className="[display:inline_list-item]" key={i}>
+                {tool}
+                {i < projMeta.tools.length - 1 && ", "}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="col-span-2">
+        <span className="font-bold text-gray-9">Date</span>
+        <p>{projMeta.date}</p>
+      </div>
+    </>
   );
 };
 
