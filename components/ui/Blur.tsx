6c57ff331d94ffbcb81fd5df6c0b26047f62c339
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-01 23:36:39 +0000

    feat: Blur component allows for more usecases

--- components/ui/Blur.tsx
+++ components/ui/Blur.tsx
@@ -1,10 +1,18 @@
-import type { ReactNode } from "react";
+import { cx } from "class-variance-authority";
+import { HTMLProps } from "react";
 
-export const Blur = ({ children }: { children: ReactNode }) => (
-  <div
-    tabIndex={0}
-    className="text-gray-3 hover:text-gray-11 focus:text-gray-11 filter blur-sm  hover:blur-none focus:blur-none duration-moderate-02 transition-all ease-productive-standard group/blur"
-  >
-    {children}
-  </div>
+export const Blur = ({
+  tabIndex,
+  className,
+  focusable = true,
+  ...rest
+}: { focusable?: boolean } & HTMLProps<HTMLSpanElement>) => (
+  <span
+    tabIndex={tabIndex ?? (focusable ? 0 : -1)}
+    className={cx(
+      className,
+      "text-gray-3 hover:text-gray-11 focus-within:text-gray-11 filter blur-sm  hover:blur-none focus:blur-none focus:text-gray-11 focus-within:blur-none duration-moderate-02 transition-all ease-productive-standard group/blur"
+    )}
+    {...rest}
+  />
 );
