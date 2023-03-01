049e8b667380b08027dbedef43e97130d1cadab4
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-01 10:17:18 +0000

    refactor: remove unused component

--- app/work/appdirbebop/MDXRemoteWrapper.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export const MDXRemoteWrapper = (props: MDXRemoteProps) => {
-  return <MDXRemote {...props} />;
-};
-
-import { MDXRemote, type MDXRemoteProps } from "next-mdx-remote";
