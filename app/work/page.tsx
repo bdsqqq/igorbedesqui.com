bdacb89d026947c79dec52551be1bd8baf108982
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-17 18:46:12 +0000

    feat: get projects list styling from home

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -2,9 +2,10 @@ import { bebopMeta, ibmMeta, issMeta, psykipMeta, wasmGifMeta } from "./metas";
 
 import Container from "@/components/Container";
 import Link from "next/link";
-import { grid } from "@/components/ui/Grid";
+import { grid, subGrid } from "@/components/ui/Grid";
 import Band from "@/components/Band";
 import { cx } from "class-variance-authority";
+import { UnstyledLink } from "@/components/ui/primitives";
 
 const projects = [ibmMeta, psykipMeta, bebopMeta, issMeta, wasmGifMeta];
 
@@ -16,22 +17,40 @@ export default function Page() {
           <div className="col-span-6">
             <h1 className="font-semibold text-3xl">Work</h1>
           </div>
-          <div className="col-start-1 md:col-start-2 lg:col-start-3 col-span-full">
-            <ul className="group hover:text-gray-8 focus-within:text-gray-8">
-              {projects.map((p) => (
-                <li key={p.urlSlug} className="">
-                  <Link
-                    className="flex flex-col py-3 px-4 -mx-4 hover:text-gray-12 hover:bg-gray-A3 focus:text-gray-12 focus:bg-gray-A3 transition-colors"
-                    href={`/work/${p.urlSlug}`}
+          <div className="col-span-full">
+            <ul
+              className={cx(
+                subGrid({
+                  sm: 4,
+                  md: 8,
+                  lg: 16,
+                })(),
+                "hover:text-gray-10 focus-within:text-gray-10 pointer-events-none"
+              )}
+            >
+              {projects.map((p) => {
+                return (
+                  <li
+                    className="col-span-4 lg:col-span-8 -mx-4 px-0 [&>a]:px-4"
+                    key={p.shortName}
                   >
-                    <h3 className="flex justify-between">
-                      <span className="font-semibold">{p.name}</span>
-                      <span>{p.date}</span>
-                    </h3>
-                    <p className="block md:w-2/3">{p.description}</p>
-                  </Link>
-                </li>
-              ))}
+                    <UnstyledLink
+                      className="block py-4 pointer-events-auto hover:text-gray-12 focus:text-gray-12 transition-colors duration-fast-02 ease-productive-standard group"
+                      href={`/work/${p.urlSlug}`}
+                    >
+                      <div className="flex flex-col ">
+                        <div>
+                          <h2 className="inline-block font-bold">{p.name}</h2>{" "}
+                          <span className="inline-block text-xs text-bold tracking-tighter text-gray-11 text-end md:opacity-0 group-hover:opacity-100 group-focus:opacity-100 transform md:-translate-x-4 group-hover:translate-x-0 group-focus:translate-x-0 transition-all duration-moderate-01 ease-productive-standard">
+                            {p.date}
+                          </span>
+                        </div>
+                        <p>{p.description}</p>
+                      </div>
+                    </UnstyledLink>
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
