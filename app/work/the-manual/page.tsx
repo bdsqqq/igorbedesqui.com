03ccd5bd67ce75f1625e5f7b34512c25cc50a48a
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-22 02:14:20 +0000

    feat: rough draft of section 2

--- app/work/the-manual/page.tsx
+++ app/work/the-manual/page.tsx
@@ -10,6 +10,7 @@ import type { Metadata } from "next";
 
 import { psykipMeta, bebopMeta } from "../metas";
 import { MDX } from "@/components/MDX";
+import Image from "next/image";
 
 const makeSeo = ({
   title,
@@ -84,12 +85,41 @@ export default async function WasmGif() {
           <MDX>
             {`
                The Enchiridion is a fascinating, easy read filled with valuable insights. Unfortunately—even with multiple public domain translations—reading it is often a bad experience.
+               <br />
 
                Between old and sometimes confusing language, hard to find and even harder to download files, and outdated websites, there are too many barriers between an interested reader and the book.            
             `}
           </MDX>
         </ProjectBand>
         <ProjectBand headline={{ bold: "02", thin: "How" }}>
+          <MDX>
+            {`
+              While the content is the focal point of the site, its presentation is how we solved the problems listed above. Balancing the tension between the simplicity of “just text on a page” and the limitless possibilities of the web, I choose to take inspiration from print and offer an *almost* “Just text” experience, where interactive elements fade in as you reach for them but don’t disturb your reading.
+              <br />
+
+              The “Reading progress” flow best exemplifies these interactive elements; Nothing is said as the user reads a translation, but when they visit the homepage again, a subtle text indicates their reading progress. If they choose to read the same translation, an option to continue from where they left off appears. My goal was to make this a “wow moment” inspired by video games that sets the tone of the site.
+              <br />
+            `}
+          </MDX>
+          <video autoPlay={false} controls={true}>
+            <source
+              src="/videos/the-manual/reading-progress.webm"
+              type="video/webm"
+            />
+            <source
+              src="/videos/the-manual/reading-progress.mp4"
+              type="video/mp4"
+            />
+          </video>
+          <div className="mt-4">
+            <MDX>
+              {`
+                *Development slowed down as this is a side project, but the recent dark theme update follows the same “show, don’t tell” pattern, and a “highlights” feature is planned in the [public roadmap](https://psykip-git-next-bdsq.vercel.app/roadmap).*
+                <br />
+              `}
+            </MDX>
+          </div>
+
           <MDX
             components={{
               ol: (props) => (
@@ -101,11 +131,78 @@ export default async function WasmGif() {
             }}
           >
             {`
-              1. I focused my efforts on design. While the content is the reason I built this site, it's user experience that solves the problems listed above. The way we allow you to interact with the Enchiridion is why this is the best way to read it.
-              1. Even though some translations aren't the best for everyone, they might be my favorites, so instead of pushing for one or another, I made it easy to read the one that works for you.
-              1. Books are hard to share, and with multiple file types that work for different devices with different applications, a project like this can grow out of control fast. That's why I took a step back and decided to serve the content from each translation in a way that's easy to read and share; nothing more, nothing less.
+              The visual design of the site takes the aforementioned inspiration from print further, exaggerating its aesthetic and feel.
+              <br />
+
+              1. No “button-y” buttons, clickable elements with clearly defined borders and backgrounds felt out of place in a book, instead, I made deliberate use of underlined text for links and icons for various interactions.
+              2. No HUD-like menu, the site needs navigation but it felt right to put links along the body of the document, not in a bar or buttons that follow the user. I can see the user for an always reachable “back” button, but the browser already provides one.
+              3. Links have directional arrows, going to translations is represented by pointing to the right, and going towards the landing page is represented by pointing to the left, mimicking the turn of pages in a book, forward to the content, and back to the index.
+              4. No pure black or white,  shades of tinted gray feel more real, and with a grainy overlay achieve a paper-like look.
+              <br />
+
+              Various forms of print also influenced the layouts of the site:
+              <br />
             `}
           </MDX>
+
+          <div className="flex flex-col">
+            <figure>
+              <Image
+                alt=""
+                src={"/images/projs/the-manual/1.jpg"}
+                width={685}
+                height={993}
+              />
+              <figcaption>The landing page mimics a book cover.</figcaption>
+            </figure>
+
+            <figure>
+              <Image
+                alt=""
+                src={"/images/projs/the-manual/2.jpg"}
+                width={685}
+                height={993}
+              />
+              <figcaption>
+                The chapters borrow from actual books with generous line
+                heights, chapter numbers and drop caps
+              </figcaption>
+            </figure>
+
+            <figure>
+              <Image
+                alt=""
+                src={"/images/projs/the-manual/3.jpg"}
+                width={1505}
+                height={993}
+              />
+              <Image
+                alt=""
+                src={"/images/projs/the-manual/4.jpg"}
+                width={1505}
+                height={993}
+              />
+              <figcaption>
+                The About and Sources pages use layouts inspired by magazines
+                with their horizontal spread and use of whitespace.
+              </figcaption>
+            </figure>
+
+            <figure>
+              <Image
+                alt=""
+                src={"/images/projs/the-manual/5.jpg"}
+                width={1505}
+                height={993}
+              />
+              <figcaption>
+                The “compare” pages were inspired by newspapers’s approach to
+                dense information in using columns, adapted to the infinite
+                scrollable canvas of the web to include more whitespace and
+                wider columns.
+              </figcaption>
+            </figure>
+          </div>
         </ProjectBand>
         <div className="hidden">
           <ProjectBand headline={{ bold: "03", thin: "Design" }}>
