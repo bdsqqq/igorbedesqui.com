4a9795f8a71a4178cb607fd490381551cc741ea5
Igor Bedesqui <igorbedesqui@gmail.com>
2023-03-23 03:32:34 +0000

    feat: styling + structure

--- app/work/the-manual/page.tsx
+++ app/work/the-manual/page.tsx
@@ -93,160 +93,168 @@ export default async function WasmGif() {
           </MDX>
         </ProjectBand>
         <ProjectBand headline={{ bold: "02", thin: "How" }}>
-          <MDX>
-            {`
-              While the content is the focal point of the site, its presentation is how we solved the problems listed above. Balancing the tension between the simplicity of “just text on a page” and the limitless possibilities of the web, I choose to take inspiration from print and offer an *almost* “Just text” experience, where interactive elements fade in as you reach for them but don’t disturb your reading.
-              <br />
-
-              The “Reading progress” flow best exemplifies these interactive elements; Nothing is said as the user reads a translation, but when they visit the homepage again, a subtle text indicates their reading progress. If they choose to read the same translation, an option to continue from where they left off appears. My goal was to make this a “wow moment” inspired by video games that sets the tone of the site, “things will work without getting in your way”.
-              <br />
-            `}
-          </MDX>
-          <video autoPlay={false} controls={true}>
-            <source
-              src="/videos/the-manual/reading-progress.webm"
-              type="video/webm"
-            />
-            <source
-              src="/videos/the-manual/reading-progress.mp4"
-              type="video/mp4"
-            />
-          </video>
-          <div className="mt-4">
-            <MDX>
+          <div className="space-y-4">
+            <MDX
+              components={{
+                h3: (props) => (
+                  <h3 className="mb-4 text-2xl font-bold" {...props} />
+                ),
+              }}
+            >
               {`
-                *Development slowed down as this is a side project, but the recent dark theme update follows the same “show, don’t tell” pattern, and a “highlights” feature is planned in the [public roadmap](https://psykip-git-next-bdsq.vercel.app/roadmap).*
-                <br />
+                ### Design direction
+
+                While the content is the focal point of the site, its presentation is how we solved the problems listed above. Balancing the tension between the simplicity of “just text on a page” and the limitless possibilities of the web, I choose to take inspiration from print and offer an *almost* “Just text” experience, where interactive elements fade in as you reach for them but don’t disturb your reading.
+
+                The “Reading progress” flow best exemplifies these interactive elements; Nothing is said as the user reads a translation, but when they visit the homepage again, a subtle text indicates their reading progress. If they choose to read the same translation, an option to continue from where they left off appears. My goal was to make this a “wow moment” inspired by video games that sets the tone of the site, “things will work without getting in your way”.
               `}
             </MDX>
-          </div>
+            <video autoPlay={false} controls={true}>
+              <source
+                src="/videos/the-manual/reading-progress.webm"
+                type="video/webm"
+              />
+              <source
+                src="/videos/the-manual/reading-progress.mp4"
+                type="video/mp4"
+              />
+            </video>
+            <div className="-mx-2 rounded border border-gray-A2 bg-gradient-to-b from-gray-A3 to-gray-A2 p-2 text-gray-9">
+              <MDX>
+                {`
+                *Development slowed down as this is a side project, but the recent dark theme update follows the same “show, don’t tell” pattern, and a “highlights” feature is planned in the [public roadmap](https://psykip-git-next-bdsq.vercel.app/roadmap).*
+              `}
+              </MDX>
+            </div>
 
-          <MDX
-            components={{
-              ol: (props) => (
-                <ol
-                  className="list-outside list-decimal space-y-2 marker:text-gray-8"
-                  {...props}
-                />
-              ),
-            }}
-          >
-            {`
+            <MDX
+              components={{
+                h3: (props) => (
+                  <h3 className="mb-4 text-2xl font-bold" {...props} />
+                ),
+                ol: (props) => (
+                  <ol
+                    className="list-outside list-decimal space-y-2 marker:text-gray-8"
+                    {...props}
+                  />
+                ),
+              }}
+            >
+              {`
+              ### Visual design
+
               The visual design of the site takes the aforementioned inspiration from print further, exaggerating its aesthetic and feel.
-              <br />
 
               1. No “button-y” buttons, clickable elements with clearly defined borders and backgrounds felt out of place in a book, instead, I made deliberate use of underlined text for links and icons for various interactions.
               2. No HUD-like menu, the site needs navigation but it felt right to put links along the body of the document, not in a bar or buttons that follow the user. I can see the user for an always reachable “back” button, but the browser already provides one.
               3. Links have directional arrows, going to translations is represented by pointing to the right, and going towards the landing page is represented by pointing to the left, mimicking the turn of pages in a book, forward to the content, and back to the index.
               4. No pure black or white,  shades of tinted gray feel more real, and with a grainy overlay achieve a paper-like look.
-              <br />
 
               Various forms of print also influenced the layouts of the site:
-              <br />
             `}
-          </MDX>
+            </MDX>
 
-          <div className="flex flex-col">
-            <figure>
-              <Image
-                alt=""
-                src={"/images/projs/the-manual/1.jpg"}
-                width={685}
-                height={993}
-              />
-              <figcaption>The landing page mimics a book cover.</figcaption>
-            </figure>
-
-            <figure>
-              <Image
-                alt=""
-                src={"/images/projs/the-manual/2.jpg"}
-                width={685}
-                height={993}
-              />
-              <figcaption>
-                The chapters borrow from actual books with generous line
-                heights, chapter numbers and drop caps
-              </figcaption>
-            </figure>
-
-            <figure>
-              <Image
-                alt=""
-                src={"/images/projs/the-manual/3.jpg"}
-                width={1505}
-                height={993}
-              />
-              <Image
-                alt=""
-                src={"/images/projs/the-manual/4.jpg"}
-                width={1505}
-                height={993}
-              />
-              <figcaption>
-                The About and Sources pages use layouts inspired by magazines
-                with their horizontal spread and use of whitespace.
-              </figcaption>
-            </figure>
-
-            <figure>
-              <Image
-                alt=""
-                src={"/images/projs/the-manual/5.jpg"}
-                width={1505}
-                height={993}
-              />
-              <figcaption>
-                The “compare” pages were inspired by newspapers’s approach to
-                dense information in using columns, adapted to the infinite
-                scrollable canvas of the web to include more whitespace and
-                wider columns.
-              </figcaption>
-            </figure>
-          </div>
-        </ProjectBand>
-        <ProjectBand headline={{ bold: "04", thin: "Development" }}>
-          <MDX
-            components={{
-              h3: (props) => <h3 className="text-2xl font-bold" {...props} />,
-              Popover: (props) => (
-                <PopOver
-                  options={{
-                    padding: "none",
-                  }}
-                  content={
-                    <figure className="space-y-1 pb-2">
-                      <Image
-                        alt=""
-                        src={"/images/projs/the-manual/kindle.jpg"}
-                        width={540}
-                        height={720}
-                      />
-                      <figcaption className="text-center">
-                        <MDX>{`[More pics on Twitter](https://twitter.com/bedesqui/status/1579608624514871296?s=20)`}</MDX>
-                      </figcaption>
-                    </figure>
-                  }
-                  {...props}
+            <div className="flex flex-col">
+              <figure>
+                <Image
+                  alt=""
+                  src={"/images/projs/the-manual/1.jpg"}
+                  width={685}
+                  height={993}
                 />
-              ),
-            }}
-          >
-            {`
-              ### Technical details for nerds
+                <figcaption>The landing page mimics a book cover.</figcaption>
+              </figure>
 
-              Before writing any user-facing code, I spent some time [playing with golang to transform 8 translations into 424 markdown files](https://twitter.com/bedesqui/status/1557388112032137218?s=20). Then, [wrote other scripts to bulk-edit these files and their metadata as needed](https://twitter.com/bedesqui/status/1560750830302760965?s=20).
-              <br />
-              
-              I approached development from a static-first angle, the site is usable with javascript disabled and on pretty much any device, <Popover>*it even ran on my kindle*</Popover>. Leveraging [Astro](astro.build/) I generated hundreds of pages for translations, chapters, and comparisons using data from the markdown files to populate a few templates. For the interactive bits, I used islands of [Solid.js](https://www.solidjs.com/) and scripts with no framework. Deploying on [Vercel](vercel.com/) meant sub-minute build times even with 490 pages, including the monstrous [“compare all translations” page](psykip.vercel.app/compare/all) with 13200 DOM elements. Additionally, [tailwindcss](https://tailwindcss.com/) allowed me to colocate styles and markup without sacrificing functionality. Other relevant technical details include:
-              
-              - Prefetching of next pages based on hover, resulting in snappy navigation for an MPA**Multi page application.
-              - Relocation of analytics script to web workers via [Partytown](https://partytown.builder.io/) and Build time image optimization to speed up the site.
-              <br />
+              <figure>
+                <Image
+                  alt=""
+                  src={"/images/projs/the-manual/2.jpg"}
+                  width={685}
+                  height={993}
+                />
+                <figcaption>
+                  The chapters borrow from actual books with generous line
+                  heights, chapter numbers and drop caps
+                </figcaption>
+              </figure>
 
-              For further details, refer to [the source code available on GitHub](https://github.com/bdsqqq/psykip/).
-            `}
-          </MDX>
+              <figure>
+                <Image
+                  alt=""
+                  src={"/images/projs/the-manual/3.jpg"}
+                  width={1505}
+                  height={993}
+                />
+                <Image
+                  alt=""
+                  src={"/images/projs/the-manual/4.jpg"}
+                  width={1505}
+                  height={993}
+                />
+                <figcaption>
+                  The About and Sources pages use layouts inspired by magazines
+                  with their horizontal spread and use of whitespace.
+                </figcaption>
+              </figure>
+
+              <figure>
+                <Image
+                  alt=""
+                  src={"/images/projs/the-manual/5.jpg"}
+                  width={1505}
+                  height={993}
+                />
+                <figcaption>
+                  The “compare” pages were inspired by newspapers’s approach to
+                  dense information in using columns, adapted to the infinite
+                  scrollable canvas of the web to include more whitespace and
+                  wider columns.
+                </figcaption>
+              </figure>
+            </div>
+
+            <MDX
+              components={{
+                h3: (props) => (
+                  <h3 className="mb-4 text-2xl font-bold" {...props} />
+                ),
+                Popover: (props) => (
+                  <PopOver
+                    options={{
+                      padding: "none",
+                    }}
+                    content={
+                      <figure className="space-y-1 pb-2">
+                        <Image
+                          alt=""
+                          src={"/images/projs/the-manual/kindle.jpg"}
+                          width={540}
+                          height={720}
+                        />
+                        <figcaption className="text-center">
+                          <MDX>{`[More pics on Twitter](https://twitter.com/bedesqui/status/1579608624514871296?s=20)`}</MDX>
+                        </figcaption>
+                      </figure>
+                    }
+                    {...props}
+                  />
+                ),
+              }}
+            >
+              {`
+                ### Technical details for nerds
+
+                Before writing any user-facing code, I spent some time [playing with golang to transform 8 translations into 424 markdown files](https://twitter.com/bedesqui/status/1557388112032137218?s=20). Then, [wrote other scripts to bulk-edit these files and their metadata as needed](https://twitter.com/bedesqui/status/1560750830302760965?s=20).
+                
+                I approached development from a static-first angle, the site is usable with javascript disabled and on pretty much any device, <Popover>*it even ran on my kindle*</Popover>. Leveraging [Astro](astro.build/) I generated hundreds of pages for translations, chapters, and comparisons using data from the markdown files to populate a few templates. For the interactive bits, I used islands of [Solid.js](https://www.solidjs.com/) and scripts with no framework. Deploying on [Vercel](vercel.com/) meant sub-minute build times even with 490 pages, including the monstrous [“compare all translations” page](psykip.vercel.app/compare/all) with 13200 DOM elements. Additionally, [tailwindcss](https://tailwindcss.com/) allowed me to colocate styles and markup without sacrificing functionality. Other relevant technical details include:
+                
+                - Prefetching of next pages based on hover, resulting in snappy navigation for an MPA**Multi page application.
+                - Relocation of analytics script to web workers via [Partytown](https://partytown.builder.io/) and Build time image optimization to speed up the site.
+
+                For further details, refer to [the source code available on GitHub](https://github.com/bdsqqq/psykip/).
+              `}
+            </MDX>
+          </div>
         </ProjectBand>
         <ProjectBand headline={{ bold: "05", thin: "Results" }}>
           <MDX>
