799deb2b958e7fce4f8ce1441107ab827ec6159a
Igor Bedesqui <igorbedesqui@gmail.com>
2023-04-14 22:34:55 +0100

    pushed this further

--- app/work/the-manual/page.tsx
+++ app/work/the-manual/page.tsx
@@ -66,11 +66,13 @@ export const metadata: Metadata = makeSeo({
 });
 
 const g = grid({ mode: "narrow" });
+const slottedInMiddle =
+  "col-start-1 col-end-5 md:col-start-2 md:col-end-7 lg:col-start-4 lg:col-end-13";
 
 export default async function TheManual() {
   return (
     <Container key="the-manual" backable backMessage={psykipMeta.backMessage}>
-      {/* <HeroBand heroVideo="/videos/the-manual/cat">
+      <HeroBand heroVideo="/videos/the-manual/cat">
         <span className="text-gray-12">
           The best way to read the Enchiridion.
         </span>
@@ -81,17 +83,18 @@ export default async function TheManual() {
           />
         </div>
       </HeroBand>
-      <div className="mb-16" /> */}
 
       <div className="flex flex-col gap-20">
         <Band gridless id="why">
           <div className={cx(g)}>
-            <div className="col-span-5 space-y-2 text-xl lg:col-span-7 lg:col-start-5">
+            <div className="col-span-5 space-y-8 text-2xl lg:col-span-12 lg:col-start-2 lg:mb-12 lg:mt-16">
               <MDX>
                 {`
                The Enchiridion is a fascinating, easy read filled with valuable insights. Unfortunately—even with multiple public domain translations—reading it is often a bad experience.
                
-               Between old and sometimes confusing language, hard to find and even harder to download files, and outdated websites, there are too many barriers between an interested reader and the book.            
+               Between old and sometimes confusing language, hard to find and even harder to download files, and outdated websites, there are too many barriers between an interested reader and the book.         
+               
+               [**The Manual**](https://psykip.vercel.app) addresses these issues trough a carefully crafted web experience, enabling frictionless reading of a variety of translations.
                `}
               </MDX>
             </div>
@@ -116,23 +119,23 @@ export default async function TheManual() {
               `}
               </MDX>
             </div>
-            <div className="col-span-5 md:col-start-2 lg:col-start-5 lg:col-end-13">
+            <div className="col-span-6 md:col-start-1 lg:col-start-1 lg:col-end-14">
               <video autoPlay={false} controls={true}>
                 <source
-                  src="/videos/the-manual/reading-progress.webm"
+                  src="/videos/the-manual/reading_progress.webm"
                   type="video/webm"
                 />
                 <source
-                  src="/videos/the-manual/reading-progress.mp4"
+                  src="/videos/the-manual/reading_progress.mp4"
                   type="video/mp4"
                 />
               </video>
             </div>
-            <div className="col-start-13 col-end-17">
-              <div className="-mx-2 rounded border border-gray-A2 bg-gradient-to-b from-gray-A3 to-gray-A2 p-2 text-gray-9">
+            <div className="col-start-14 col-end-17 flex items-end">
+              <div className="-mx-2 h-fit rounded border border-gray-A2 bg-gradient-to-b from-gray-A3 to-gray-A2 p-2 text-gray-9">
                 <MDX>
                   {`
-                      *Development slowed down as this is a side project, but the recent dark theme update follows the same “show, don’t tell” pattern, and a “highlights” feature is planned in the [public roadmap](https://psykip-git-next-bdsq.vercel.app/roadmap).*
+                      *The recent dark theme update follows the same “show, don’t tell” pattern by choosing a theme based purely on system preferences, and a “highlights” feature is planned in the [public roadmap](https://psykip-git-next-bdsq.vercel.app/roadmap).*
                     `}
                 </MDX>
               </div>
@@ -141,30 +144,39 @@ export default async function TheManual() {
         </Band>
         <Band gridless id="visual">
           <div className={cx(g, "space-y-4")}>
-            <h3 className="col-start-1 col-end-13 mb-4 text-2xl font-bold lg:col-start-5">
+            <h3 className={cx(slottedInMiddle, "mb-4 text-2xl font-bold")}>
               Visual design
             </h3>
-            <p className="col-start-1 col-end-13 inline-block lg:col-start-5">
-              {`The visual design of the site takes the aforementioned inspiration from print further, exaggerating its aesthetic and feel.`}
+            <p className={cx(slottedInMiddle, "inline-block")}>
+              {`The visual design of the site takes the aforementioned inspiration from print further, exaggerating its aesthetic and feel by working within carefully built constraints.`}
             </p>
             <MDX
               components={{
                 ol: (props) => (
                   <ol
-                    className="col-start-1 col-end-13 list-outside list-decimal space-y-2 marker:text-gray-8 lg:col-start-4"
+                    className={cx(
+                      subGrid({ lg: 14, md: 6, sm: 4 })(),
+                      "col-span-4 list-inside list-decimal gap-y-4 marker:text-gray-8 md:col-start-2 md:col-end-8 lg:col-start-4 lg:col-end-14"
+                    )}
+                    {...props}
+                  />
+                ),
+                li: (props) => (
+                  <li
+                    className="col-span-4 md:col-span-3 lg:col-span-7"
                     {...props}
                   />
                 ),
               }}
             >
               {`
-              1. No “button-y” buttons, clickable elements with clearly defined borders and backgrounds felt out of place in a book, instead, I made deliberate use of underlined text for links and icons for various interactions.
-              1. No HUD-like menu, the site needs navigation but it felt right to put links along the body of the document, not in a bar or buttons that follow the user. I can see the user for an always reachable “back” button, but the browser already provides one.
               1. Links have directional arrows, going to translations is represented by pointing to the right, and going towards the landing page is represented by pointing to the left, mimicking the turn of pages in a book, forward to the content, and back to the index.
+              1. No HUD-like menu, the site needs navigation but it felt right to put links along the body of the document, not in a bar or buttons that follow the user. I can see the user for an always reachable “back” button, but the browser already provides one.
+              1. No “button-y” buttons, clickable elements with clearly defined borders and backgrounds felt out of place in a book, instead, I made deliberate use of underlined text for links and icons for various interactions.
               1. No pure black or white,  shades of tinted gray feel more real, and with a grainy overlay achieve a paper-like look.
               `}
             </MDX>
-            <p className="col-start-1 col-end-16 inline-block">
+            <p className="col-start-1 col-end-5 inline-block md:col-start-2 md:col-end-4 lg:col-start-5 lg:col-end-13">
               {`Various forms of print also influenced the layouts of the site:`}
             </p>
 
@@ -202,7 +214,7 @@ export default async function TheManual() {
 
               <figure
                 className={cx(
-                  "col-span-12 col-start-4",
+                  "col-span-12 col-start-2",
                   subGrid({
                     lg: 12,
                     md: 8,
@@ -279,7 +291,7 @@ export default async function TheManual() {
               </figure>
             </div>
 
-            <div className="col-span-16">
+            <div className={cx(slottedInMiddle, "space-y-4")}>
               <MDX
                 components={{
                   h3: (props) => (
@@ -313,10 +325,11 @@ export default async function TheManual() {
 
                 Before writing any user-facing code, I spent some time [playing with golang to transform 8 translations into 424 markdown files](https://twitter.com/bedesqui/status/1557388112032137218?s=20). Then, [wrote other scripts to bulk-edit these files and their metadata as needed](https://twitter.com/bedesqui/status/1560750830302760965?s=20).
                 
-                I approached development from a static-first angle, the site is usable with javascript disabled and on pretty much any device, <Popover>*it even ran on my kindle*</Popover>. Leveraging [Astro](astro.build/) I generated hundreds of pages for translations, chapters, and comparisons using data from the markdown files to populate a few templates. For the interactive bits, I used islands of [Solid.js](https://www.solidjs.com/) and scripts with no framework. Deploying on [Vercel](vercel.com/) meant sub-minute build times even with 490 pages, including the monstrous [“compare all translations” page](psykip.vercel.app/compare/all) with 13200 DOM elements. Additionally, [tailwindcss](https://tailwindcss.com/) allowed me to colocate styles and markup without sacrificing functionality. Other relevant technical details include:
+                I approached development from a static-first angle, the site is usable with javascript disabled and on pretty much any device, <Popover>*it even ran on my kindle*</Popover>. Leveraging [Astro](astro.build/) I generated hundreds of pages for translations, chapters, and comparisons using data from the markdown files to populate a few templates. 
                 
-                - Prefetching of next pages based on hover, resulting in snappy navigation for an MPA**Multi page application.
-                - Relocation of analytics script to web workers via [Partytown](https://partytown.builder.io/) and Build time image optimization to speed up the site.
+                For the interactive bits, I used islands of [Solid.js](https://www.solidjs.com/) and scripts with no framework. Deploying on [Vercel](vercel.com/) meant sub-minute build times even with 490 pages, including the monstrous [“compare all translations” page](psykip.vercel.app/compare/all) with 13200 DOM elements. Additionally, [tailwindcss](https://tailwindcss.com/) allowed me to colocate styles and markup without sacrificing functionality. 
+                
+                Other relevant technical details include: Prefetching of next pages based on user interaction, resulting in snappy navigation for an Multi Page Application,  relocation of analytics script to web workers via [Partytown](https://partytown.builder.io/), and build time image optimization to speed up the site.
 
                 For further details, refer to [the source code available on GitHub](https://github.com/bdsqqq/psykip/).
               `}
