fcc904d24bd38b69c12fa671af0fa02b84a8e3c2
Igor Bedesqui <igorbedesqui@gmail.com>
2023-04-16 03:24:42 +0100

    forgot to delete this references to separator

--- components/ui/primitives/index.tsx
+++ components/ui/primitives/index.tsx
@@ -1,5 +1,4 @@
 import { default as FABContainer } from "./FABContainer";
 import { default as UnstyledLink } from "./UnstyledLink";
-import { default as Separator } from "./Separator";
 
-export { FABContainer, UnstyledLink, Separator };
+export { FABContainer, UnstyledLink };
