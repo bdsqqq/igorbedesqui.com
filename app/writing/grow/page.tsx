05d1d0eeb849fb4260b378bce75e45d8531e8282
Igor Bedesqui <igorbedesqui@gmail.com>
2023-04-29 23:55:28 +0100

    grow

---
+++ app/writing/grow/page.tsx
@@ -0,0 +1,25 @@
+import { MDX } from "@/components/MDX";
+
+export default function Page() {
+  return (
+    <MDX>
+      {`
+         Isn't this what you wanted?
+
+         To grow?
+
+         You wish to be kind so you are tested with negativity.
+
+         You wish to be strong so you are tested with weakness.
+
+         You wish to love so you are tested with loneliness.
+
+         The universe doesn't work like you do. You wask for something, it provides you with to prove you deserve what you are asking for.
+
+         Why do you think people stay the same their entire lives. To elevate is extremely hard. It is significantly easier to not improve yourself.
+
+         It's a tough journey on your mind, body and souk. But I believe in you. Seek the infinite potential within you.
+      `}
+    </MDX>
+  );
+}
