be79bf85242b09a42c565256017589ab321b9d0e
Igor Bedesqui <igorbedesqui@gmail.com>
2023-05-09 11:57:37 +0100

    add minesweeper metadata to work/metas

--- app/work/metas.ts
+++ app/work/metas.ts
@@ -95,3 +95,16 @@ export const psykipMeta: Meta = {
   urlSlug: "the-manual",
   draft: false,
 };
+
+export const minesweeperMeta: Meta = {
+  shortName: "minesweeper",
+  name: "Minesweeper!",
+  description: "A modern implementation of the classic game minesweeper",
+  roles: ["FrontEnd Developer", "UX/UI Designer"],
+  type: "Personal",
+  tools: [],
+  date: "Oct 2021",
+  urlSlug: "minesweeper",
+  backMessage: "",
+  draft: true,
+};
