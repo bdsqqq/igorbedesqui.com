2635c89abcbe5152e073faedaafc84de3c5296db
Igor Bedesqui <igorbedesqui@gmail.com>
2023-05-11 00:15:31 +0100

    move Think2022 to appdir

---
+++ app/work/ibm/Think-2022/page.tsx
@@ -0,0 +1,36 @@
+import HeroBand from "@/components/HeroBand";
+import CodeAndDemoButtons from "@/components/ProjectStuff/CodeAndDemoButtons";
+import ProjectContainer from "@/components/ProjectStuff/ProjectContainer";
+import {
+  ProjectBand,
+  ProjectLayout,
+} from "@/components/ProjectStuff/ProjectLayout";
+import PopOver from "@/components/ui/Popover";
+import type { Metadata } from "next";
+
+import { bebopMeta, issMeta } from "../../metas";
+import { MDX } from "@/components/MDX";
+import Image from "next/image";
+
+import desktopScreenshot from "@/public/images/projs/bebop/desktop-screenshot.png";
+import smartphoneScreenshot from "@/public/images/projs/bebop/smartphone-screenshot.png";
+
+export default async function Think() {
+  return (
+    <ProjectContainer key="bebopProj" backMessage={bebopMeta.backMessage}>
+      <HeroBand heroVideo={"/videos/bebop/noodles"}>
+        <MDX>
+          {`
+            Leading bleeding edge at massive scale.
+          `}
+        </MDX>
+      </HeroBand>
+      <div className="mb-16" />
+
+      <ProjectLayout
+        projMeta={bebopMeta}
+        nextProjMeta={issMeta}
+      ></ProjectLayout>
+    </ProjectContainer>
+  );
+}
