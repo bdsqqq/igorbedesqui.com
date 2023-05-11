bbad3debc5cb47fabbe8ab7b79958a6fcf4f8c05
Igor Bedesqui <igorbedesqui@gmail.com>
2023-05-11 15:47:07 +0100

    expose colors as css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -184,5 +184,29 @@ module.exports = {
       variants: ["lg", "md"],
     },
   ],
-  plugins: [require("tailwindcss-radix")()],
+  plugins: [
+    require("tailwindcss-radix")(),
+    function ({ addBase, theme }) {
+      function extractColorVars(colorObj, colorGroup = "") {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey];
+          const cssVariable =
+            colorKey === "DEFAULT"
+              ? `--color${colorGroup}`
+              : `--color${colorGroup}-${colorKey}`;
+
+          const newVars =
+            typeof value === "string"
+              ? { [cssVariable]: value }
+              : extractColorVars(value, `-${colorKey}`);
+
+          return { ...vars, ...newVars };
+        }, {});
+      }
+
+      addBase({
+        ":root": extractColorVars(theme("colors")),
+      });
+    },
+  ],
 };
