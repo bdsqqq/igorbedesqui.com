3f4c14a494d3acad38a4ab0c6278fccd6f6ddb6f
Igor Bedesqui <igorbedesqui@gmail.com>
2023-05-19 01:03:27 +0100

    fix test page

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -4,31 +4,13 @@ import {
   CVAWithPerms
 } from "@/lib/CVAPermutations";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
-
-import { variantOutsideCva} from "@/ui/Button"
-import { cva } from "class-variance-authority";
-//        ^?
-
-
-const [a, b] = CVAWithPerms("", variantOutsideCva);
-//     ^?
-
-const c = cva("", variantOutsideCva);
-//    ^?
-
-if(a) {
-  a()
-}
-
-c({
-
-})
+import { cn } from "@/lib/styling";
 
 function replaceLeafValuesWithDots(obj: Record<string, any>): Record<string, any> {
   const result: Record<string, any> = {};
 
   for (const key in obj) {
-    if (typeof obj[key] === "object" && obj[key] !== null) {
+    if (typeof obj[key] === "object" && obj[key] !== null && Array.isArray(obj[key]) === false) {
       result[key] = replaceLeafValuesWithDots(obj[key]);
     } else {
       result[key] = "...";
@@ -38,23 +20,45 @@ function replaceLeafValuesWithDots(obj: Record<string, any>): Record<string, any
   return result;
 }
 
-const variantsPlaceholder = replaceLeafValuesWithDots(variantOutsideCva);
+const cvaConfig = {
+  variants: {
+    size: {
+      sm: "text-sm",
+      md: "text-base",
+      lg: "text-xl",
+    },
+    intent: {
+      primary: [
+        "border-gray-A4",
+        "shadow-gray-A3",
+        "from-gray-A2 to-gray-A4",
+        "hover:border-gray-A8",
+        "focus-visible:border-gray-A8"
+      ]
+    },
+  },
+}
+
+const [variants, permutations] = CVAWithPerms(
+  cn(
+    "select-none appearance-none",
+    "relative rounded-sm px-2 py-1 inline-flex items-center gap-2 align-middle outline-none focus-within:outline-none",
+    "motion-safe:duration-fast-01 motion-safe:ease-expressive-standard transition-all",
+    "border",
+    "shadow-input ",
+    "bg-gradient-to-tr",
+    "active:scale-95",
+    "before:absolute before:inset-0 before:rounded before:shadow-lg before:shadow-gray-0/50 before:transition-all before:motion-safe:duration-fast-02 before:motion-safe:ease-expressive-standard"
+  ),
+  cvaConfig
+);
+
+const variantsPlaceholder = replaceLeafValuesWithDots(cvaConfig);
 
 
 const Page = () => {
   return (
     <div className="grid min-h-screen place-items-center px-6">
-      {/* <StagesWrap>
-        <Stage title="Hej do">
-          <Button>Hej do</Button>
-        </Stage>
-        <Stage title="Hej do">
-          <Button>Hej do</Button>
-        </Stage>
-        <Stage title="Hej do">
-          <Button>Hej do</Button>
-        </Stage>
-      </StagesWrap> */}
       <div className="flex gap-6 items-center">
         <div className="max-w-sm">
           <pre className="">{JSON.stringify(variantsPlaceholder, null, 2)}</pre>
@@ -63,15 +67,15 @@ const Page = () => {
           <ArrowRightIcon />
         </div>
         <div className="">
-          <pre>{JSON.stringify(b, null, 2)}</pre>
+          <pre>{JSON.stringify(permutations, null, 2)}</pre>
         </div>
         <div className="w-4 h-4 text-base">
           <ArrowRightIcon />
         </div>
         <StagesWrap>
-          {b && b.map((x) => (
-          <Stage key={`${Object.values(x).join(" ")}`} className="" title={`${Object.values(x).join(" ")}`}>
-            <Button {...x}>Hej do</Button>
+          {permutations && permutations.map((permutation) => (
+          <Stage key={`${Object.values(permutation).join(" ")}`} className="" title={`${Object.values(permutation).join(" ")}`}>
+            <Button {...permutation}>Hej do</Button>
           </Stage>
           ))}
         </StagesWrap>
