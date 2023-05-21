0d7fd5beca287b85f03bd883ded4eb99c147ff8b
Igor Bedesqui <igorbedesqui@gmail.com>
2023-05-21 18:24:13 +0100

    Unstyled Link proper ref passing and named export

--- components/ui/primitives/UnstyledLink.tsx
+++ components/ui/primitives/UnstyledLink.tsx
@@ -1,29 +1,22 @@
-const UnstyledLink = ({
-  className,
-  href,
-  children,
-  ...rest
-}: LinkProps & HtmlHTMLAttributes<{}>) => {
-  const isInternalLink =
-    typeof href == "string" && (href.startsWith("/") || href.startsWith("#"));
+export type UnstyledLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & LinkProps
+
+export const UnstyledLink = React.forwardRef<HTMLAnchorElement, UnstyledLinkProps>(({ className, href, ...rest }, ref) => {
+  if (!href) return (<a className={className} {...rest} ref={ref} />)
+
+  const isInternalLink = (href.startsWith("/") || href.startsWith("#") || typeof href === "object"); // href can be an object if it is an URLObject from NextJS
   if (isInternalLink) {
     return (
-      <Link href={href} passHref className={className} {...rest}>
-        {children}
-      </Link>
+      <Link href={href} passHref className={className} {...rest} ref={ref} />
     );
   }
 
   return (
-    // eslint-disable-next-line react/jsx-no-target-blank
-    <a className={className} target="_blank" href={href as string} {...rest}>
-      {children}
-    </a>
+    <a className={className} target="_blank" href={href} {...rest} ref={ref} />
   );
-};
+})
 
 export default UnstyledLink;
 
+import React from "react";
 import Link from "next/link";
 import type { LinkProps } from "next/link";
-import type { HtmlHTMLAttributes } from "react";
