62c146308bc218a0a409cd382668d86efcaf7593
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-10 22:00:40 +0100

    feat: no need for z-index

--- components/ui/newPopover.tsx
+++ components/ui/newPopover.tsx
@@ -37,7 +37,7 @@ export { Popover, PopoverTrigger, PopoverContent };
 
 const tooltipVariants = cva(
   [
-    "z-50 w-72 rounded shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 duration-fast-02 data-[state=closed]:ease-productive-exit data-[state=open]:ease-productive-enter data-[state=closed]:opacity-0 data-[state=open]:opacity-100 transition-opacity",
+    "w-72 rounded shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 duration-fast-02 data-[state=closed]:ease-productive-exit data-[state=open]:ease-productive-enter data-[state=closed]:opacity-0 data-[state=open]:opacity-100 transition-opacity",
   ],
   {
     variants: {
