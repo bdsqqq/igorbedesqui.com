d498374d1a427941d87130dea943fb3b856dc869
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-21 03:43:29 +0100

    feat: make Popover use border classes instead of HoC

--- components/ui/Popover.tsx
+++ components/ui/Popover.tsx
@@ -5,7 +5,6 @@ import * as PopoverPrimitive from "@radix-ui/react-popover";
 
 import { cn } from "@/lib/styling";
 import { VariantProps, cva } from "class-variance-authority";
-import { Border } from "./Border";
 
 const Popover = PopoverPrimitive.Root;
 
@@ -18,18 +17,29 @@ const PopoverContent = React.forwardRef<
       options?: VariantProps<typeof tooltipVariants>;
     }
   >
->(({ className, align = "end", sideOffset = 4, options, ...props }, ref) => (
-  <Border>
-    {/* not using PopoverPrimitive.Portal to wrap everything because it fucks up the exit animation */}
+>(
+  (
+    {
+      className,
+      align = "center",
+      side = "top",
+      sideOffset = 4,
+      options,
+      ...props
+    },
+    ref
+    // not using PopoverPrimitive.Portal to wrap everything because it fucks up the exit animation
+  ) => (
     <PopoverPrimitive.Content
       ref={ref}
       align={align}
       sideOffset={sideOffset}
+      side={side}
       className={cn(tooltipVariants(options), className)}
       {...props}
     />
-  </Border>
-));
+  )
+);
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
 
 export { Popover, PopoverTrigger, PopoverContent };
@@ -37,7 +47,8 @@ export { Popover, PopoverTrigger, PopoverContent };
 const tooltipVariants = cva(
   [
     `
-    w-72 rounded shadow-md outline-none
+    rounded-sm shadow-md outline-none
+    border border-gray-A4
     data-[state=open]:animate-in data-[state=closed]:animate-out
     data-[state=closed]:ease-productive-exit data-[state=open]:ease-productive-enter data-[state=open]:duration-fast-01 data-[state=closed]:duration-fast-01 data-[state=open]:zoom-in-95 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95  origin-radix-popover data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1 data-[side=bottom]:slide-out-to-top-1 data-[side=left]:slide-out-to-right-1 data-[side=right]:slide-out-to-left-1 data-[side=top]:slide-out-to-bottom-1
     `,
