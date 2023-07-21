27314efd2e380e8a89bb0f5d3e79463d41663b06
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-21 03:55:12 +0100

    feat: Lock use new Popover properly

--- components/ui/Lock.tsx
+++ components/ui/Lock.tsx
@@ -1,36 +1,38 @@
-import Popover from "@/ui/Popover";
-import React from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "@/ui/Popover";
+import React, {
+  Children,
+  cloneElement,
+  forwardRef,
+  isValidElement,
+} from "react";
 
 /**
  * @see: https://github.com/radix-ui/primitives/pull/2234#issuecomment-1613000587
  */
-export const Lock = React.forwardRef<
+export const Lock = forwardRef<
   HTMLButtonElement,
   React.PropsWithChildren<{
     locked: boolean;
     lockedFeedback: React.ReactNode;
   }>
 >(({ children, locked, lockedFeedback, ...rest }, ref) => {
-  const child = React.Children.only(children);
+  const child = Children.only(children);
   if (!locked) return <>{child}</>;
 
   // Not too happy about this bit, but it makes sure cloneElement gets a valid element (not a string and other misc types allowed by ReactNode)
-  const isValid = React.isValidElement<
-    HTMLButtonElement & { onClick?: () => void }
-  >(child);
+  const isValid = isValidElement<HTMLButtonElement & { onClick?: () => void }>(
+    child
+  );
   if (!isValid)
-    throw new Error(
-      `LockWithoutSlot's child must be a valid react element, see: `
-    );
+    throw new Error(`Lock's child must be a valid react element, see: `);
 
   return (
-    <Popover
-      content={lockedFeedback}
-      Icon={React.Fragment}
-      // TODO: react fragment can't receive className, so I can't really have this with NO icon. Make it optional somehow.
-    >
-      {React.cloneElement(child, { onClick: () => {} })}
+    <Popover>
+      <PopoverTrigger asChild {...rest} ref={ref}>
+        {cloneElement(child, { onClick: () => {} })}
+      </PopoverTrigger>
+      <PopoverContent align="end">{lockedFeedback}</PopoverContent>
     </Popover>
   );
 });
-Lock.displayName = "LockWithoutSlot";
+Lock.displayName = "Lock";
