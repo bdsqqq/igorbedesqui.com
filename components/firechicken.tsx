d41f50f0757d9cd740fe0237e412c2c3bedc187a
Igor Bedesqui <igorbedesqui@gmail.com>
2023-12-24 14:32:50 +0000

    feat: new style for fire chicken thing

--- components/firechicken.tsx
+++ components/firechicken.tsx
@@ -1,14 +1,22 @@
-import { UnstyledLink } from "@/components/ui/primitives";
 import StyledLink from "@/ui/StyledLink";
 
 export default function FireChicken() {
   return (
-    <div className="flex gap-1">
-      <UnstyledLink href="https://firechicken.club/igor/prev">←</UnstyledLink>
-      <StyledLink href="https://firechicken.club">
+    <div className="inline-flex gap-1">
+      <StyledLink iconless href="https://firechicken.club">
         Fire Chicken Webring
       </StyledLink>
-      <UnstyledLink href="https://firechicken.club/igor/next">→</UnstyledLink>
+      <span className="inline-flex gap-0.5 text-gray-9">
+        (
+        <StyledLink iconless href="https://firechicken.club/igor/prev">
+          prev
+        </StyledLink>
+        /
+        <StyledLink iconless href="https://firechicken.club/igor/next">
+          next
+        </StyledLink>
+        )
+      </span>
     </div>
   );
 }
