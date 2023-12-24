98ad7670660bc939f58e6406e02eb8407b32e263
Igor Bedesqui <igorbedesqui@gmail.com>
2023-12-24 15:38:39 +0000

    feat: fix typo, had the author 2 times

--- app/writing/schrodinger-minimalism/page.tsx
+++ app/writing/schrodinger-minimalism/page.tsx
@@ -69,7 +69,7 @@ export default async function Basics() {
 
                > “Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away.”
                > 
-               > <div className="text-sm before:content-['—'] flex gap-1 justify-end w-full"><Popover><cite>Antoine de Saint-Exupéry Antoine de Saint-Exupéry</cite></Popover></div>
+               > <div className="text-sm before:content-['—'] flex gap-1 justify-end w-full"><Popover><cite>Antoine de Saint-Exupéry</cite></Popover></div>
                 
                While I generally agree with this idea, and use it as guidance when designing, **there’s something that is lost when we omit details**, especially when writing. YES, I can express the same general idea with way less text, but it’s not the same.
 
