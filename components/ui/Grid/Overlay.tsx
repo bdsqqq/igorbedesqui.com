fc2f3728da90ebc7f409fd894f2518fa41bdd779
Igor Bedesqui <igorbedesqui@gmail.com>
2023-12-25 16:08:28 +0000

    feat: show grid

--- components/ui/Grid/Overlay.tsx
+++ components/ui/Grid/Overlay.tsx
@@ -12,7 +12,6 @@ export const Overlay = () => {
             setVisible((v) => !v);
           }}
           className="h-8"
-          size="sm"
         >
           {visible ? <EyeOpenIcon /> : <EyeClosedIcon />}
         </Button>
@@ -41,6 +40,31 @@ export const Overlay = () => {
                 <div className="relative h-full w-full bg-gray-A2 text-center text-gray-10">
                   <div className="sticky top-4 left-0 right-0">{i + 1}</div>
                 </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className={cn("pointer-events-none absolute inset-0")}>
+        <div
+          className={cn(grid({ mode: mode }), "h-full w-full px-4 md:px-16")}
+        >
+          {[...Array(16)].map((_, i) => {
+            return (
+              <div
+                key={`${i}-always_visible`}
+                className={cn(
+                  "hidden border-l border-dashed border-gray-A1",
+                  i < 4 && "block",
+                  i + 1 == 4 && "border-r md:border-r-0",
+                  i < 8 && "md:block",
+                  i + 1 == 8 && "md:border-r lg:border-r-0",
+                  i < 16 && "lg:block",
+                  i + 1 == 16 && "lg:border-r"
+                )}
+              >
+                <div className="relative h-full w-full border-x border-dashed border-gray-A2" />
               </div>
             );
           })}
