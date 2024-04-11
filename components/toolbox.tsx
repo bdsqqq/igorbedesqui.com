003e12f65c68dee0465736345b41af998f2c89e9
Igor Bedesqui <igorbedesqui@gmail.com>
2024-04-11 11:49:27 +0100

    feat: portal devtools

--- components/toolbox.tsx
+++ components/toolbox.tsx
@@ -7,22 +7,45 @@ import {
   PopoverTrigger,
 } from "@/components/ui/Popover";
 import { InPortal, OutPortal } from "@/components/ui/Portal";
+import { PortalDevtools } from "@/components/ui/Portal/devtools";
 import { cn } from "@/lib/styling";
-import { MixerHorizontalIcon } from "@radix-ui/react-icons";
-import { ReactNode } from "react";
+import {
+  OpenInNewWindowIcon,
+} from "@radix-ui/react-icons";
+import { ReactNode, useCallback, useState } from "react";
 
 const TOOLBOX_PORTAL_NAME = "toolbox";
 
 export const Toolbox = () => {
+  const [isDebugging, setIsDebugging] = useState(false);
+  const toggleDebugging = useCallback(() => {
+    setIsDebugging((prev) => !prev);
+  }, []);
+
   return (
-    <div
-      className={cn(
-        "fixed right-4 top-4 z-50",
-        "has-[data-state='open']: opacity-0 transition-opacity duration-fast-02 ease-productive-standard focus-within:opacity-100 hover:opacity-100",
-      )}
-    >
-      <OutPortal name={TOOLBOX_PORTAL_NAME} />
-    </div>
+    <>
+      <ToolboxItem name="portal-debugging">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button onClick={toggleDebugging} className="h-8">
+              <OpenInNewWindowIcon />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent align="end">
+            <PortalDevtools />
+          </PopoverContent>
+        </Popover>
+      </ToolboxItem>
+
+      <div
+        className={cn(
+          "fixed right-4 top-4 z-50",
+          `opacity-0 transition-opacity duration-fast-02 ease-productive-standard focus-within:opacity-100 hover:opacity-100 has-[button[data-state="open"]]:opacity-100`,
+        )}
+      >
+        <OutPortal name={TOOLBOX_PORTAL_NAME} />
+      </div>
+    </>
   );
 };
 
--- components/ui/Portal/devtools.tsx
+++ components/ui/Portal/devtools.tsx
@@ -9,45 +9,43 @@ export function PortalDevtools() {
   const { inPortals, outPortals } = useStore(PortalStore);
 
   return (
-    <div className="border-gray-05 bg-gray-03 fixed bottom-4 left-4 rounded border p-2">
-      <div>
-        <h2>Portals</h2>
-        <div className="flex gap-2">
-          <div>
-            <h3>out</h3>
-            <ul>
-              {outPortals.map((p) => {
-                const linked = inPortals.find((i) => i.intendedOut === p.name);
+    <div>
+      <h2>Portals</h2>
+      <div className="flex gap-2">
+        <div>
+          <h3>out</h3>
+          <ul>
+            {outPortals.map((p) => {
+              const linked = inPortals.find((i) => i.intendedOut === p.name);
 
-                return (
-                  <li
-                    className={cn(linked ? "text-[green]" : "text-[orange]")}
-                    key={p.id}
-                  >
-                    {p.name}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+              return (
+                <li
+                  className={cn(linked ? "text-gray-12" : "text-gray-11")}
+                  key={p.id}
+                >
+                  {p.name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
 
-          <div>
-            <h3>in</h3>
-            <ul>
-              {inPortals.map((p) => {
-                const linked = outPortals.find((o) => o.name === p.intendedOut);
+        <div>
+          <h3>in</h3>
+          <ul>
+            {inPortals.map((p) => {
+              const linked = outPortals.find((o) => o.name === p.intendedOut);
 
-                return (
-                  <li
-                    className={cn(linked ? "text-[green]" : "text-[orange]")}
-                    key={p.id}
-                  >
-                    {p.name}
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+              return (
+                <li
+                  className={cn(linked ? "text-gray-12" : "text-gray-11")}
+                  key={p.id}
+                >
+                  {p.name}
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </div>
