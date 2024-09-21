afe4bd2282ba4c30796b09189ebb4269b86a1b57
Igor Bedesqui <igorbedesqui@gmail.com>
2024-09-21 12:54:27 +0100

    feat: don't use absolute black, rely on theme

--- components/ui/Dialog.tsx
+++ components/ui/Dialog.tsx
@@ -21,7 +21,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      "fixed inset-0 bg-[#000000]/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 bg-gray-00 opacity-70  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className,
     )}
     {...props}
