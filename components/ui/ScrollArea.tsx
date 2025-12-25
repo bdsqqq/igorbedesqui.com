cd2d1fdf003a742e1c1d5d2e7fe30ad2f885f2fa
Igor Bedesqui <igorbedesqui@gmail.com>
2025-12-25 01:02:09 +0000

    refactor(ui): remove forwardRef from ScrollArea and UnstyledLink
    react 19 allows ref as a regular prop

--- components/ui/ScrollArea.tsx
+++ components/ui/ScrollArea.tsx
@@ -5,44 +5,60 @@ import { ScrollArea as ScrollAreaPrimitive } from "radix-ui";
 
 import { cn } from "@/lib/styling";
 
-const ScrollArea = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => (
-  <ScrollAreaPrimitive.Root
-    ref={ref}
-    className={cn("relative overflow-hidden", className)}
-    {...props}
-  >
-    <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
-      {children}
-    </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
-    <ScrollAreaPrimitive.Corner />
-  </ScrollAreaPrimitive.Root>
-));
+function ScrollArea({
+  className,
+  children,
+  ref,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> & {
+  ref?: React.Ref<React.ElementRef<typeof ScrollAreaPrimitive.Root>>;
+}) {
+  return (
+    <ScrollAreaPrimitive.Root
+      ref={ref}
+      className={cn("relative overflow-hidden", className)}
+      {...props}
+    >
+      <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
+        {children}
+      </ScrollAreaPrimitive.Viewport>
+      <ScrollBar />
+      <ScrollAreaPrimitive.Corner />
+    </ScrollAreaPrimitive.Root>
+  );
+}
 ScrollArea.displayName = ScrollAreaPrimitive.Root.displayName;
 
-const ScrollBar = React.forwardRef<
-  React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
->(({ className, orientation = "vertical", ...props }, ref) => (
-  <ScrollAreaPrimitive.ScrollAreaScrollbar
-    ref={ref}
-    orientation={orientation}
-    className={cn(
-      "flex touch-none select-none transition-colors",
-      orientation === "vertical" &&
-        "h-full w-2.5 border-l border-l-transparent p-[1px]",
-      orientation === "horizontal" &&
-        "h-2.5 flex-col border-t border-t-transparent p-[1px]",
-      className,
-    )}
-    {...props}
-  >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="bg-gray-09 relative flex-1 rounded-full" />
-  </ScrollAreaPrimitive.ScrollAreaScrollbar>
-));
+function ScrollBar({
+  className,
+  orientation = "vertical",
+  ref,
+  ...props
+}: React.ComponentPropsWithoutRef<
+  typeof ScrollAreaPrimitive.ScrollAreaScrollbar
+> & {
+  ref?: React.Ref<
+    React.ElementRef<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>
+  >;
+}) {
+  return (
+    <ScrollAreaPrimitive.ScrollAreaScrollbar
+      ref={ref}
+      orientation={orientation}
+      className={cn(
+        "flex touch-none select-none transition-colors",
+        orientation === "vertical" &&
+          "h-full w-2.5 border-l border-l-transparent p-[1px]",
+        orientation === "horizontal" &&
+          "h-2.5 flex-col border-t border-t-transparent p-[1px]",
+        className,
+      )}
+      {...props}
+    >
+      <ScrollAreaPrimitive.ScrollAreaThumb className="bg-gray-09 relative flex-1 rounded-full" />
+    </ScrollAreaPrimitive.ScrollAreaScrollbar>
+  );
+}
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName;
 
 const ScrollAreaCorner = ScrollAreaPrimitive.ScrollAreaCorner;
--- components/ui/primitives/UnstyledLink.tsx
+++ components/ui/primitives/UnstyledLink.tsx
@@ -1,38 +1,41 @@
-export type UnstyledLinkProps = LinkProps & ComponentPropsWithoutRef<"a">;
+import Link from "next/link";
+import type { LinkProps } from "next/link";
+import type { ComponentPropsWithoutRef, Ref } from "react";
 
-const UnstyledLink = forwardRef<HTMLAnchorElement, UnstyledLinkProps>(
-  ({ className, href, children, ...rest }, ref) => {
-    const isInternalLink =
-      typeof href == "string" && (href.startsWith("/") || href.startsWith("#"));
-    if (isInternalLink) {
-      return (
-        <Link ref={ref} href={href} passHref className={className} {...rest}>
-          {children}
-        </Link>
-      );
-    }
+export type UnstyledLinkProps = LinkProps &
+  ComponentPropsWithoutRef<"a"> & {
+    ref?: Ref<HTMLAnchorElement>;
+  };
 
+function UnstyledLink({
+  className,
+  href,
+  children,
+  ref,
+  ...rest
+}: UnstyledLinkProps) {
+  const isInternalLink =
+    typeof href == "string" && (href.startsWith("/") || href.startsWith("#"));
+  if (isInternalLink) {
     return (
-      <a
-        ref={ref}
-        className={className}
-        target="_blank"
-        href={href as string}
-        {...rest}
-      >
+      <Link ref={ref} href={href} passHref className={className} {...rest}>
         {children}
-      </a>
+      </Link>
     );
-  },
-);
+  }
+
+  return (
+    <a
+      ref={ref}
+      className={className}
+      target="_blank"
+      href={href as string}
+      {...rest}
+    >
+      {children}
+    </a>
+  );
+}
 UnstyledLink.displayName = "UnstyledLink";
 
 export default UnstyledLink;
-
-import Link from "next/link";
-import type { LinkProps } from "next/link";
-import {
-  forwardRef,
-  type ComponentPropsWithoutRef,
-  type HtmlHTMLAttributes,
-} from "react";
