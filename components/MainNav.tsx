f9607519c0cd51714aa791ae3cefca01f360126e
Igor Bedesqui <igorbedesqui@gmail.com>
2026-03-21 01:42:18 +0000

    fix bad navigation styling, fix a lint

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -2,21 +2,17 @@
 
 type MainNavProps =
   | {
-    backable?: boolean;
-    backMessage?: never;
-    backAnchor?: string;
-  }
+      backable?: boolean;
+      backMessage?: never;
+      backAnchor?: string;
+    }
   | {
-    backable: true;
-    backMessage?: string;
-    backAnchor?: string;
-  };
+      backable: true;
+      backMessage?: string;
+      backAnchor?: string;
+    };
 
-function MainNav({
-  backable,
-  backMessage,
-  backAnchor,
-}: MainNavProps) {
+function MainNav({ backable, backMessage, backAnchor }: MainNavProps) {
   const { breadcrumbs } = useBreadcrumbsStore();
   const anchor = useMemo(() => {
     if (!breadcrumbs) return "/";
@@ -34,14 +30,14 @@ function MainNav({
   return (
     <nav
       className={cva(
-        "relative z-aboveVignette flex w-full items-start justify-end p-8 md:mx-auto md:px-16",
+        "relative z-aboveVignette flex w-full items-start justify-start p-8 md:mx-auto md:px-16",
         {
           variants: {
             backable: {
               backable: "justify-between",
             },
           },
-        }
+        },
       )({
         backable: backable ? "backable" : undefined,
       })}
@@ -51,7 +47,7 @@ function MainNav({
           href={anchor ? anchor : "/"}
           className="group -mx-4 flex translate-x-0 transform cursor-pointer select-none items-center gap-1 px-4 pb-0 text-xl text-gray-11 duration-moderate-01 ease-productive-standard hover:-translate-x-2 hover:text-gray-12 focus:-translate-x-2 focus:text-gray-12 motion-safe:transition-all"
         >
-          <span className="flex-shrink-0 translate-y-[1px]">
+          <span className="shrink-0 translate-y-px">
             {anchor === "/" ? <PinLeftIcon /> : <ArrowLeftIcon />}
           </span>
           <span className="ml-1 opacity-[0.01] duration-fast-02 ease-productive-standard group-hover:opacity-100 group-focus:opacity-100 motion-safe:transition-opacity">
@@ -61,7 +57,7 @@ function MainNav({
       )}
     </nav>
   );
-};
+}
 
 export default MainNav;
 
