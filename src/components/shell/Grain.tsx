26b50737196d9abd5edd4d5f5117661b63b29541
Igor Bedesqui <igorbedesqui@gmail.com>
2026-03-22 23:20:40 +0000

    spend a few hours trying out svgs for grain, static image performs better

--- src/components/shell/Grain.tsx
+++ src/components/shell/Grain.tsx
@@ -35,6 +35,12 @@ export const Grain = () => {
     ];
     let i = 0;
 
+    /**
+     * @bdsqqq @ 2026-03-22T23:13
+     * I thought it was bad to use js to animate the grain,
+     * so I tested doing it with css keyframes, it was about 10x slower.
+     * sooooo, yea.
+     */
     const interval = setInterval(() => {
       grain.style.transform = `translateX(${
         keyframesX[i % keyframesX.length]
@@ -50,7 +56,17 @@ export const Grain = () => {
     <div className="pointer-events-none fixed inset-0 z-40 h-full w-full overflow-hidden">
       <div
         ref={grainRef}
-        className="absolute inset-[-200%] h-[400%] w-[400%] bg-[length:256px] bg-left-top opacity-[3%]"
+        className="absolute inset-[-200%] h-[400%] w-[400%] bg-size-[256px] bg-top-left opacity-3"
+        /**
+         * @bdsqqq @ 2026-03-22T23:13
+         * I thought it was bad to use a static image for the grain,
+         * and while I have more control with an svg feTurbulence,
+         * it introduces annoying clipping behaviour; the browser
+         * tries to NOT render tiles outside of the viewport, but since
+         * we animate translations, we're taking tiles in and out faster
+         * than the browser can keep up with, so we end up with big squares
+         * of blank space. Can explore again later, but worth the comment
+         */
         style={{ backgroundImage: `url(${framernoiseUrl})` }}
       />
     </div>
