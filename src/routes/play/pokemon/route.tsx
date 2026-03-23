6d0e5bfdcd419f7bd5066c287166af39edb68d82
Igor Bedesqui <igorbedesqui@gmail.com>
2026-03-23 22:00:51 +0000

    move stuff around & rename it

--- src/routes/play/pokemon/route.tsx
+++ src/routes/play/pokemon/route.tsx
@@ -1,36 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 
-import pokemonSheet from "./-pokemonicons-sheet.png";
-
-const PokemonIcon = ({ id, scale = 1 }: { id: string; scale?: number }) => {
-  const SPRITESHEET_DIMENSIONS = {
-    width: 480,
-    height: 3990,
-  };
-
-  const num = parseInt(id);
-  const top = Math.floor(num / 12) * 30;
-  const left = (num % 12) * 40;
-
-  return (
-    <div
-      style={{
-        background: `transparent url(${pokemonSheet}) no-repeat scroll -${left * scale}px -${top * scale}px / ${SPRITESHEET_DIMENSIONS.width * scale}px ${SPRITESHEET_DIMENSIONS.height * scale}px`,
-        width: `${40 * scale}px`,
-        height: `${30 * scale}px`,
-        imageRendering: "pixelated",
-      }}
-    />
-  );
-};
-
 function Example() {
-  const [id, setId] = useState("885");
+  const [dex_number, setDexNumber] = useState("885");
 
   return (
     <div>
-      <PokemonIcon id={id} scale={4} />
+      <PokemonSprite dex_number={dex_number} scale={3} />
     </div>
   );
 }
@@ -51,3 +27,33 @@ export const Route = createFileRoute("/play/pokemon")({
     meta: [{ title: "Pokemon — Play — Igor Bedesqui" }],
   }),
 });
+
+import pokemonSheet from "./-pokemonicons-sheet.png";
+
+const PokemonSprite = ({
+  dex_number,
+  scale = 3,
+}: {
+  dex_number: string;
+  scale?: number;
+}) => {
+  const SPRITESHEET_DIMENSIONS = {
+    width: 480,
+    height: 3990,
+  };
+
+  const num = parseInt(dex_number);
+  const top = Math.floor(num / 12) * 30;
+  const left = (num % 12) * 40;
+
+  return (
+    <div
+      style={{
+        background: `transparent url(${pokemonSheet}) no-repeat scroll -${left * scale}px -${top * scale}px / ${SPRITESHEET_DIMENSIONS.width * scale}px ${SPRITESHEET_DIMENSIONS.height * scale}px`,
+        width: `${40 * scale}px`,
+        height: `${30 * scale}px`,
+        imageRendering: "pixelated",
+      }}
+    />
+  );
+};
